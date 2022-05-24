f033e0f9cb84a50289ca42f466628f3a9140be5d
tsannie <tsannie@student.42.fr>
2022-05-24 16:54:18 +0200

    [+] start req react to nest

--- srcs/front_react/src/App.tsx
+++ srcs/front_react/src/App.tsx
@@ -1,26 +1,6 @@
-import axios from 'axios';
 import React from 'react';
 
-const api = axios.create({
-  baseURL: 'http://localhost:4000/'
-})
-
 function App() {
-
-  let allUser = {
-    username: []
-  }
-
-  const ret = api.get('/user').then(res => {
-    return res.data;
-  })
-  console.log(ret)
-
-/*   axios.get('http://localhost:4000/user').then(res => {
-    const allUser = res.data;
-    console.log(res.data)
-  }) */
-
   return (
     <h1>Hello react !</h1>
   );
