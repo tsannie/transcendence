d362a2d95e76f61ec019aa8f4d08a0204668f3ff
tsannie <tsannie@student.42.fr>
2022-05-24 16:54:18 +0200

    [+] start req react to nest

--- srcs/front_react/src/index.tsx
+++ srcs/front_react/src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import PostList from './userlist/UserListItem';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <PostList />
   </React.StrictMode>,
   document.getElementById('root')
 )
--- srcs/front_react/src/userlist/UserListItem.tsx
+++ srcs/front_react/src/userlist/UserListItem.tsx
@@ -1,9 +1,29 @@
-import React from "react";
 
-interface User {
-	username: "string";
+import React from 'react';
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:4000/'
+})
+
+
+export default class PostList extends React.Component {
+ state = {
+  posts: []
 }
 
-export const UserListItem: React.FC<User> = (user) => {
-	return <li>content</li>
+componentDidMount() {
+  api.get('/user').then(res => {
+    const posts = res.data;
+    this.setState({ posts });
+   })
+}
+
+render() {
+  return (
+   <ul>
+    { this.state.posts.map(post => <li>{post.name}</li>)}
+   </ul>
+  )
+ }
 }
