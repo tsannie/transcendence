42c8418c460e1e5001f68366adc01b910daec824
tsannie <tsannie@student.42.fr>
2022-05-31 06:13:20 +0200

    [fix] edit to change component to function hook

--- srcs/front_react/src/userlist/UserListItem.tsx
+++ srcs/front_react/src/userlist/UserListItem.tsx
@@ -1,29 +1,39 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:4000/'
 })
 
-
-export default class PostList extends React.Component {
- state = {
-  posts: []
+export interface IUser {
+  id: number,
+  name: string,
 }
 
-componentDidMount() {
-  api.get('/user').then(res => {
-    const posts = res.data;
-    this.setState({ posts });
-   })
-}
+export default function UserList(props: IUser) {
 
-render() {
+  // Declare a new state variable
+  const [name, setName] = React.useState('');
+  const [id, setId] = React.useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate
+
+  function getUser() {
+    api.get('/user').then(res => {
+      setId(res.data.id);
+      setName(res.data.name);
+     })
+  }
+
+  useEffect(() => {
+    getUser();
+
+  });
+
   return (
    <ul>
     { this.state.posts.map(post => <li>{post.name}</li>)}
    </ul>
   )
- }
 }
