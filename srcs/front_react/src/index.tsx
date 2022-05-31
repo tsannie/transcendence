f3d3a7264dcccc4047ac89eb336c8fe35212ee4e
tsannie <tsannie@student.42.fr>
2022-05-31 06:13:20 +0200

    [fix] edit to change component to function hook

--- srcs/front_react/src/index.tsx
+++ srcs/front_react/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import PostList from './userlist/UserListItem';
+import UserList from './userlist/UserListItem';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <PostList />
+    <UserList />
   </React.StrictMode>,
   document.getElementById('root')
 )
