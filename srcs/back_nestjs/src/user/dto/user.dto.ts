bc79e3d105d271c7774f270cf8e30944c02fd6f4
tsannie <tsannie@student.42.fr>
2022-05-31 13:24:32 +0200

    [+] dto

--- srcs/back_nestjs/src/user/dto/user.dto.ts
+++ srcs/back_nestjs/src/user/dto/user.dto.ts
@@ -1,9 +1,21 @@
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { IsDefined, IsEmail, IsNotEmpty } from "class-validator";
 
 export class UserDto {
+
+  id: number;
+
+  @IsDefined()
+  @IsNotEmpty()
+  name: string;
+
+  @IsDefined()
   @IsEmail()
   email: string;
 
+  @IsDefined()
   @IsNotEmpty()
   password: string;
+
+  createdAt: Date;
+  updatedAt: Date;
 }
--- srcs/back_nestjs/src/user/models/user.interface.ts
+++ srcs/back_nestjs/src/user/models/user.interface.ts
@@ -1,8 +1,8 @@
 export interface IUser {
-	id: number,
-	name: string,
-	email: string,
-	password: string,
-	createdAt: Date,
-	updatedAt: Date
+  id: number,
+  name: string,
+  email: string,
+  password: string,
+  createdAt: Date,
+  updatedAt: Date
 }
