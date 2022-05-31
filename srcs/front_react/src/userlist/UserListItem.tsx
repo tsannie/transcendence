e1868f73725ff720b752369735a93e05197eb33b
tsannie <tsannie@student.42.fr>
2022-05-31 10:14:52 +0200

    [+] function react

--- srcs/front_react/src/userlist/UserListItem.tsx
+++ srcs/front_react/src/userlist/UserListItem.tsx
@@ -16,18 +16,22 @@ export default function UserList() {
   // Declare a new state variable
   const [name, setName] = React.useState('');
   const [id, setId] = React.useState(0);
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-  // Similar to componentDidMount and componentDidUpdate
 
   function getUser() {
     api.get('/user').then(res => {
       //console.log(res.data)
-      console.log(res.data[0])
+      console.log(res.data);
       setId(res.data[0].id);
       setName(res.data[0].name);
-     })
-    }
+      setEmail(res.data[0].email);
+      setPassword(res.data[0].password);
+    })
+  }
 
+  // Similar to componentDidMount and componentDidUpdate
     useEffect(() => {
       getUser();
   });
@@ -35,6 +39,10 @@ export default function UserList() {
   return (
     <ul>
       <li key={id}>{name}</li>
+        <ul>
+          <li>{email}</li>
+          <li>{password}</li>
+        </ul>
     </ul>
   )
 }
