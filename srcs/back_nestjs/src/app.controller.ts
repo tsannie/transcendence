9698846ca368ad853f3546c177593a56bf4d9593
tsannie <tsannie@student.42.fr>
2022-06-03 15:34:03 +0200

    [+] first login

--- srcs/back_nestjs/src/app.controller.ts
+++ srcs/back_nestjs/src/app.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
+import { AuthService } from './auth/service/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService
+  ) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    console.log('hello')
+    return req.user;
+  }
 
   @Get()
   getHello(): string {
--- srcs/back_nestjs/src/auth/controller/auth.controller.ts
+++ srcs/back_nestjs/src/auth/controller/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Post, Request } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 
-@Controller('auth')
+@Controller('')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
     //@UseGuards(LocalAuthGuard)
-    @Post('auth/login')  // login
+    /*@Post('auth/login')  // login
     async login(@Request() req) {
       return this.authService.login(req.user);
-    }
+    }*/
 }
