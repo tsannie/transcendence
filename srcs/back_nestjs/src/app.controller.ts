f7793e8d63ae49ce3535bbbc940ca9a23ba33891
tsannie <tsannie@student.42.fr>
2022-06-06 12:22:08 +0200

    [fix] switch controler

--- srcs/back_nestjs/src/app.controller.ts
+++ srcs/back_nestjs/src/app.controller.ts
@@ -1,27 +1,11 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthService } from './auth/service/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService,
-    private authService: AuthService
+    private readonly appService: AppService
   ) {}
-
-  @UseGuards(AuthGuard('local'))    // TODO switch to auth controler
-  @Post('auth/login')
-  async login(@Request() req) {
-    console.log('hello')
-    return this.authService.login(req.user);
-  }
-
-  @UseGuards(AuthGuard('jwt'))
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
 
   @Get()
   getHello(): string {
--- srcs/back_nestjs/src/auth/controller/auth.controller.ts
+++ srcs/back_nestjs/src/auth/controller/auth.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Post, Request } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from '../service/auth.service';
 
 @Controller('')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-    //@UseGuards(LocalAuthGuard)
-    /*@Post('auth/login')  // login
-    async login(@Request() req) {
-      return this.authService.login(req.user);
-    }*/
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    console.log('hello')
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
 }
