b7424c1d8506da5301bdf2113daa228fa511c1ab
tsannie <tsannie@student.42.fr>
2022-06-08 11:50:03 +0200

    [fix] form

--- srcs/back_nestjs/src/auth/controller/auth.controller.ts
+++ srcs/back_nestjs/src/auth/controller/auth.controller.ts
@@ -4,22 +4,22 @@ import { UserDto } from 'src/user/dto/user.dto';
 import { UserService } from 'src/user/service/user.service';
 import { AuthService, IToken } from '../service/auth.service';
 
-@Controller('')
+@Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService
   ) {}
 
   @UseGuards(AuthGuard('local'))
-  @Post('auth/login')
+  @Post('/login')
   async login(@Request() req): Promise<IToken> {
     console.log('hello')
     return await this.authService.login(req.user);
   }
 
-  @Post()
+  @Post('/register')
   @Header('Access-Control-Allow-Origin', '*')
-  async add(@Body() user: UserDto): Promise<UserDto> {
+  async register(@Body() user: UserDto): Promise<UserDto> {
     return await this.authService.register(user);
   }
 
