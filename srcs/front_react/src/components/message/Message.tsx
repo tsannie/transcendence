23e9266e644350fe292242afc72fc5cd8a523076
dodjian <dovdjianpro@gmail.com>
2022-06-09 17:43:07 +0200

    to do: check if user prompt in front is in db

--- srcs/front_react/src/components/message/Message.tsx
+++ srcs/front_react/src/components/message/Message.tsx
@@ -3,24 +3,21 @@ import { io, Socket } from "socket.io-client";
 
 const socket = io('http://localhost:4000');
 
-function Recv_msg() {
-  socket.on('events', (data) => {
-    console.log('msgToClient', data);
-  });
-}
-
-function Send_msg() {
-  //console.log('send_msg');
-  socket.on('connect', () => {
-    console.log(`Connected with ${socket.id}`);
-    socket.emit('events', { name: 'Nest' }, (data: string) => console.log(data));
-  });
-}
-
 function Msg() {
-  //console.log('msg');
-  Recv_msg();
-  Send_msg();
+
+  function Recv_msg() {
+    socket.on('events', (data) => {
+      console.log('msgToClient', data);
+    });
+  }
+
+  function Send_msg() {
+    //console.log('send_msg');
+    socket.on('connect', () => {
+      console.log(`Connected with ${socket.id}`);
+      socket.emit('events', { name: 'Nest' }, (data: string) => console.log(data));
+    });
+  }
   socket.on('disconnect', () => {
     console.log(`Disconnected with ${socket.id}`);
   });
@@ -29,19 +26,6 @@ function Msg() {
 
   </input>
   );
-}
-
-export function required() {
-
-  const inputValue = (document.getElementById("inputmessage") as HTMLInputElement).value
-
-  if (inputValue.length === 0)
-  {
-     alert("Nom incorrect");
-     return false;
-  }
-  return true;
-
 }
 
 export default Msg;
