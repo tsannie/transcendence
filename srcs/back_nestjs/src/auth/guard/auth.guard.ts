e5f17785c8d20d171ec8f7a50cf1014c32e6b4a5
tsannie <tsannie@student.42.fr>
2022-06-14 16:16:09 +0200

    [+] start 42 guard

---
+++ srcs/back_nestjs/src/auth/guard/auth.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class AuthGuarde implements CanActivate {
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+    console.log(req);
+    return (true);
+    //return validateRequest(request);
+  }
+}
