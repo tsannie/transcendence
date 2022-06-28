8a339382e30c0888cbfe645da7e7f2b802c61503
dodjian <dovdjianpro@gmail.com>
2022-06-28 15:02:05 +0200

    [+] css to mui chat footer done

--- srcs/front_react/src/components/chat/Chat.tsx
+++ srcs/front_react/src/components/chat/Chat.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   CssBaseline,
   darkScrollbar,
@@ -12,6 +13,7 @@ import { v4 as uuidv4 } from "uuid";
 import "./Chat.css";
 import { createTheme } from "@mui/material/styles";
 import { IMessage } from "./types";
+import Paperplane from "../../assets/paperplane.png";
 
 const socket = io("http://localhost:4000/chat");
 
@@ -60,68 +62,77 @@ export default function Chat() {
 
   if (!windowChat) {
     return (
-      <div className="chat">
-        <div className="chat-join">
-          <TextField
-            variant="outlined"
-            placeholder="username"
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-          <TextField
-            variant="outlined"
-            placeholder="room"
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
-          <Button sx={{ height: 56 }} variant="contained" onClick={createRoom}>
-            Join a room
-          </Button>
-        </div>
-      </div>
+      <Box sx={{ position: "absolute", top: 0, left: 88 }}>
+        <TextField
+          variant="outlined"
+          placeholder="username"
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <TextField
+          variant="outlined"
+          placeholder="room"
+          onChange={(event) => {
+            setRoom(event.target.value);
+          }}
+        />
+        <Button sx={{ height: 56 }} variant="contained" onClick={createRoom}>
+          Join a room
+        </Button>
+      </Box>
     );
   }
 
   return (
-    <div className="chat">
-      <div className="chat-window">
-        <div className="chat-header">
-          <p> Live chat</p>
-        </div>
-        <div className="chat-body">
-          <div className="messages-list">
-            {messagesList.map((messageData) => {
-              return (
-                <div
-                  className={
-                    author === messageData.author ? "sender" : "receiver"
-                  }
-                  key={messageData.id}
-                >
-                  {messageData.content}
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        <div className="chat-footer">
-          <TextField
-            id="standard-basic"
-            variant="outlined"
-            placeholder="Enter a message"
-            onChange={(event) => {
-              setCurrentMessage(event.target.value);
-            }}
-          />
-          <img
-            alt="send message img"
-            src={require("../../assets/paperplane.png")}
-            onClick={sendMessage}
-          ></img>
+    <div className="chat-window">
+      <div className="chat-header">
+        <p> Live chat </p>
+      </div>
+      <div className="chat-body">
+        <div className="messages-list">
+          {messagesList.map((messageData) => {
+            return (
+              <div
+                className={
+                  author === messageData.author ? "sender" : "receiver"
+                }
+                key={messageData.id}
+              >
+                {messageData.content}
+              </div>
+            );
+          })}
         </div>
       </div>
+      <Box
+        sx={{
+          width: "fit-content",
+          mx: "auto",
+          position: "relative",
+        }}
+      >
+        <TextField
+          id="standard-basic"
+          variant="outlined"
+          placeholder="Enter a message"
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+        />
+        <Box
+          component="img"
+          alt="send message img"
+          src={Paperplane}
+          onClick={sendMessage}
+          sx={{
+            width: 18,
+            height: 18,
+            position: "absolute",
+            bottom: 0,
+          }}
+        ></Box>
+      </Box>
     </div>
   );
 }
