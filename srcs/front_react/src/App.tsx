2086ac0c75e592c71578f2aaeba6363f94fccf87
dodjian <dovdjianpro@gmail.com>
2022-07-06 15:16:19 +0200

    [+] add dark mode (change png to light in d mode)

--- srcs/front_react/src/App.tsx
+++ srcs/front_react/src/App.tsx
@@ -1,22 +1,85 @@
-import { Box } from "@mui/material";
-import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  createTheme,
+  IconButton,
+  ThemeProvider,
+  Typography,
+  useTheme,
+} from "@mui/material";
+
+import React, { createContext, useContext, useMemo, useState } from "react";
 import Login from "./Auth/Login";
 import Register from "./Auth/Register";
 import Chat from "./components/chat/Chat";
 import Sidebar from "./components/sidebar/Sidebar";
 import UserList from "./userlist/UserListItem";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
 
-export default function App() {
+const ColorModeContext = createContext({ darkMode: () => {} });
+
+function App() {
   const [inputChat, setInputChat] = useState(false);
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
 
   return (
     <Box
       sx={{
         display: "flex",
+        bgcolor: "background.default",
+        color: "text.primary",
+        borderRadius: 1,
       }}
     >
+      <Button
+        sx={{
+          height: "fit-content",
+          position: "absolute",
+          right: 0,
+        }}
+        onClick={colorMode.darkMode}
+        color="inherit"
+      >
+        {theme.palette.mode === "dark" ? (
+          <Brightness7Icon />
+        ) : (
+          <Brightness4Icon />
+        )}
+      </Button>
       <Sidebar inputChat={inputChat} setInputChat={setInputChat} />
       {inputChat && <Chat />}
     </Box>
+  );
+}
+
+export default function Darkmode() {
+  const [mode, setMode] = useState<"light" | "dark">("light");
+  const colorMode = useMemo(
+    () => ({
+      darkMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
