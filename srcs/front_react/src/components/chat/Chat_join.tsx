2306fdc0b4c52bfe55ce78b63f446d27cb6f67a2
dodjian <dovdjianpro@gmail.com>
2022-07-06 17:44:09 +0200

    [+] start channel

---
+++ srcs/front_react/src/components/chat/Chat_join.tsx
@@ -0,0 +1,29 @@
+import { Box, Button, TextField } from "@mui/material";
+import React from "react";
+
+/* function Chat_join() {
+
+  return (
+    <Box sx={{ position: "absolute", top: 0, left: 88 }}>
+        <TextField
+          variant="outlined"
+          placeholder="username"
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <TextField
+          variant="outlined"
+          placeholder="room"
+          onChange={(event) => {
+            setRoom(event.target.value);
+          }}
+        />
+        <Button sx={{ height: 56 }} variant="contained" onClick={createRoom}>
+          Join a room
+        </Button>
+      </Box>
+  )
+} */
+
+//export default Chat_join
