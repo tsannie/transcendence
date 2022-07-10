6c8f1b46f28bb289dba8aeb98426605170852a06
Dov Djian <dovdjianpro@gmail.com>
2022-07-10 19:01:10 +0200

    [+] NOT WORKING. to do: logout btn with sidebar

--- srcs/front_react/src/Auth/Login.tsx
+++ srcs/front_react/src/Auth/Login.tsx
@@ -1,11 +1,13 @@
 import axios from "axios";
-import React from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { REDIRECT_LINK_AUTH } from "../const";
 import { api } from "../userlist/UserListItem";
 
-export default function ButtonLogin() {
+export default function ButtonLogin(props: any) {
   function linkLog(event: any) {
+    props.isLogin(true);
+    console.log("dans login")
     event.preventDefault();
     window.location.href = REDIRECT_LINK_AUTH;
   }
