822fad77935568ef2d05b8a1b52b3a69da3ac377
Dov Djian <dovdjianpro@gmail.com>
2022-07-18 17:24:45 +0200

    link new message with history conv

--- srcs/front_react/src/components/chat/messages/HistoryMessages.tsx
+++ srcs/front_react/src/components/chat/messages/HistoryMessages.tsx
@@ -1,12 +1,40 @@
-import { Grid } from '@mui/material'
-import React from 'react'
+import { Grid, Typography } from "@mui/material";
+import React from "react";
 
 export default function HistoryMessages(props: any) {
   return (
-    <Grid sx={{
-
-    }}>
-      History conv
+    <Grid
+      alignItems="left"
+      container
+      direction="column"
+      sx={{
+        maxWidth: "fit-content",
+        maxHeight: "fit-content",
+      }}
+    >
+      <Typography
+        sx={{
+          fontWeight: "bold",
+        }}
+        variant="h6"
+      >
+        Conv history
+      </Typography>
+      <Grid
+        item
+        sx={{
+          display: { xs: props.isNewMessage ? "block" : "none" },
+        }}
+      >
+        <Typography
+          sx={{
+            fontWeight: "semi-bold",
+          }}
+          variant="subtitle1"
+        >
+          Conv 1
+        </Typography>
+      </Grid>
     </Grid>
-  )
+  );
 }
