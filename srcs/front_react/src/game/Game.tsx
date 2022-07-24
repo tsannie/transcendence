61df600e5b6a1bbabe4ebe8ca2e7a961a2822a3a
philippebarradas <philippe.lb.barradas@gmail.com>
2022-07-24 13:46:56 +0200

    [SAVE] LOG

--- srcs/front_react/src/game/Game.tsx
+++ srcs/front_react/src/game/Game.tsx
@@ -184,10 +184,6 @@ export default function Game() {
 
 
         <p style={{ color: "red" }}> {isfull} </p>
-
-        <p> nbr connect = {nbrconnect}</p>
-        <p> conect {conect} </p>
-        <p> conect {conect} </p>
       </div>
     );
   }
