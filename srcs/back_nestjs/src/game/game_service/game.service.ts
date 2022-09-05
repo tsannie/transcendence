9654fa4be4efb28390c240402b665c15fdcf9519
philippebarradas <philippe.lb.barradas@gmail.com>
2022-09-05 14:02:00 +0200

    [SAVE] clean code

--- srcs/back_nestjs/src/game/game_service/game.service.ts
+++ srcs/back_nestjs/src/game/game_service/game.service.ts
@@ -5,25 +5,25 @@ import { GameEntity } from '../game_entity/game.entity';
 
 @Injectable()
 export class GameService {
-    constructor(
-      @InjectRepository(GameEntity)
-      private all_game: Repository<GameEntity>,
-    ) {}
+  constructor(
+    @InjectRepository(GameEntity)
+    private all_game: Repository<GameEntity>,
+  ) {}
 
-/*     getHello(): string {
+  /*     getHello(): string {
         return 'Hello GAN!';
     } */
 
-    findAll(): Promise<GameEntity[]> {
-        return this.all_game.find();
-    }
+  findAll(): Promise<GameEntity[]> {
+    return this.all_game.find();
+  }
 
-    findByName(room_name : string) : Promise<GameEntity> {
-        return this.all_game.findOne({room_name})
-    }
+  findByName(room_name: string): Promise<GameEntity> {
+    return this.all_game.findOne({ room_name });
+  }
 
-    async deleteUser(id: number): Promise<void> {
-        await this.all_game.delete(id);
-    }
+  async deleteUser(id: number): Promise<void> {
+    await this.all_game.delete(id);
+  }
 }
 //
--- srcs/front_react/src/index.tsx
+++ srcs/front_react/src/index.tsx
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import Darkmode from "./App";
 import App from './App';
-import Register from './Auth/Register';
 import Game from './game/Game';
 
 import UserList from './userlist/UserListItem';
-import App from "./App";
 import Chat from "./components/chat/Chat";
 import PostList from "./userlist/UserListItem";
 
