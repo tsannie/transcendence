7882942b72ec3795342cb8baac78e68f0a38bf8b
dodjian <dovdjianpro@gmail.com>
2022-09-05 17:29:37 +0200

    [+] change logout button for cookies

--- srcs/front_react/src/index.tsx
+++ srcs/front_react/src/index.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import Darkmode from "./App";
-import ButtonLogin from './Auth/Login';
-import ButtonLogout from './Auth/Logout';
 import UserList from './userlist/UserListItem';
 import App from "./App";
 import Chat from "./components/chat/Chat";
 import PostList from "./userlist/UserListItem";
-import Login from "./Auth/Login";
 
 const root = createRoot(document.getElementById("root")!);
 
