b7ec798ef4b6c3967f6a3b20cc7597d8d8619cfe
philippebarradas <philippe.lb.barradas@gmail.com>
2022-09-06 16:15:15 +0200

    [SAVE] div whole game work

--- srcs/front_react/src/game/Game.tsx
+++ srcs/front_react/src/game/Game.tsx
@@ -19,17 +19,24 @@ import {
   wait,
 } from "@testing-library/user-event/dist/utils";
 import { resolve } from "path";
-import GameJoined_left, { GameJoined_right } from "./GameJoined";
-import GameCreation from "./GameCreation";
-import GameWaitReady from "./GameCreation";
+import {The_whole_game, GameStarted_left, GameStarted_right } from "./GameStarted";
+import {Creation_game, GameCreation, GameWaitReady} from "./GameCreation";
 import GameSpectator from "./GameSpectator";
 import GameInit from "./GameInit";
 
-const socket = io("http://localhost:4000/game");
+export const socket = io("http://localhost:4000/game");
 
 socket.on("connect_error", (err) => {
   console.log(`|||||||||||connect_error due to ${err.message}`);
 });
+
+export let {
+  ballObj,
+  player_left,
+  player_right,
+  paddleProps_left,
+  paddleProps_right,
+} = data;
 
 export default function Game() {
   const [nbrconnect, setnbrconnect] = useState(0);
@@ -55,16 +62,10 @@ export default function Game() {
 
   const [listGame, setListGame] = useState<string[]>([]);
 
-  let {
-    ballObj,
-    player_left,
-    player_right,
-    paddleProps_left,
-    paddleProps_right,
-  } = data;
 
   function createGameRoom() {
-    if (room == "") setPP_empty("INVALID ROOM NAME");
+    if (room == "")
+      setPP_empty("INVALID ROOM NAME");
     else if (isinroom == false) {
       socket.emit("createGameRoom", room);
     }
@@ -214,176 +215,26 @@ export default function Game() {
     });
   }, [socket]);
 
-  useEffect(() => {
-    socket.on("sincTheBall", (theroom) => {
-      ballObj.x = theroom.set.ball.x;
-      ballObj.y = theroom.set.ball.y;
 
-      ballObj.ingame_dx = theroom.set.ball.ingame_dx;
-      ballObj.ingame_dy = theroom.set.ball.ingame_dy;
+  Creation_game();
 
-      ballObj.init_dx = theroom.set.ball.init_dx;
-      ballObj.init_dy = theroom.set.ball.init_dy;
-
-      ballObj.init_first_dx = theroom.set.ball.init_first_dx;
-      ballObj.init_first_dy = theroom.set.ball.init_first_dy;
-
-      ballObj.first_dx = theroom.set.ball.first_dx;
-      ballObj.first_dy = theroom.set.ball.first_dy;
-
-      ballObj.init_ball_pos = theroom.set.ball.init_ball_pos;
-      ballObj.first_col = theroom.set.ball.first_col;
-    });
-    socket.on("mouvPaddleLeft", (theroom) => {
-      paddleProps_left.x = theroom.set.p1_padle_obj.x;
-      paddleProps_left.y = theroom.set.p1_padle_obj.y;
-    });
-    socket.on("mouvPaddleRight", (theroom) => {
-      paddleProps_right.x = theroom.set.p2_padle_obj.x;
-      paddleProps_right.y = theroom.set.p2_padle_obj.y;
-    });
-    socket.on("setPlayerLeft", (theroom) => {
-      player_left.score = theroom.set.set_p1.score;
-      player_left.won = theroom.set.set_p1.won;
-      player_left.name = theroom.set.set_p1.name;
-    });
-    socket.on("setPlayerRight", (theroom) => {
-      player_right.score = theroom.set.set_p2.score;
-      player_right.won = theroom.set.set_p2.won;
-      player_right.name = theroom.set.set_p2.name;
-    });
-  }, [socket]);
-
-  function sinc_ball(room_name: string, objball: any) {
-    if (player_left.won == false && player_right.won == false) {
-      var data = {
-        room: room_name,
-        ball: objball,
-      };
-
-      console.log(" !!!!! sinc ball  EMIT in  sincBall===!!!!");
-      socket.emit("sincBall", data);
-    }
-  }
-
-  function mouv_paddle_left(e: any) {
-    if (
-      nbrconnect == 2 &&
-      isinroom &&
-      opready == true &&
-      imready == true &&
-      im_right == false &&
-      player_left.won == false &&
-      player_right.won == false
-    ) {
-      paddleProps_left.y = e.clientY - paddleProps_left.width / 2 - 15;
-      var data = {
-        room: room,
-        pd: paddleProps_left,
-      };
-      socket.emit("paddleMouvLeft", data);
-    }
-  }
-
-  function mouv_paddle_right(e: any) {
-    if (
-      nbrconnect == 2 &&
-      isinroom &&
-      opready == true &&
-      imready == true &&
-      im_right == true &&
-      player_left.won == false &&
-      player_right.won == false
-    ) {
-      paddleProps_right.y = e.clientY - paddleProps_right.width / 2 - 15;
-      var data = {
-        room: room,
-        pd: paddleProps_right,
-      };
-      socket.emit("paddleMouvRight", data);
-    }
-  }
 
   const canvasRef = useRef(null);
-  let u = 0;
-  useEffect(() => {
-    socket.on("startGame", (theroom) => {
-      player_left.name = theroom.set.set_p1.name;
-      player_right.name = theroom.set.set_p2.name;
-      sinc_ball(theroom.room_name, ballObj);
-
-      const render = () => {
-        const canvas: any = canvasRef.current;
-        var ctx = null;
-        if (canvas) ctx = canvas.getContext("2d");
-
-        if (ctx) {
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-          if (player_left.won == false && player_right.won == false) {
-            draw_line(ctx, ballObj, canvas.height, canvas.width);
-            draw_score(
-              ctx,
-              player_left,
-              player_right,
-              canvas.height,
-              canvas.width
-            );
-            BallMouv(ctx, ballObj, canvas.height, canvas.width);
-            BallCol_left(
-              ctx,
-              player_right,
-              ballObj,
-              paddleProps_left,
-              canvas.height,
-              canvas.width
-            );
-            if (ballObj.is_col == true) u = 1;
-            BallCol_right(
-              ctx,
-              player_left,
-              ballObj,
-              paddleProps_right,
-              canvas.height,
-              canvas.width
-            );
-            if (ballObj.is_col == true || ballObj.init_ball_pos == false) u = 1;
-            if (u > 0) u++;
-            if (u == 6) {
-              sinc_ball(theroom.room_name, ballObj);
-              u = 0;
-            }
-            PaddleMouv_left(ctx, canvas, paddleProps_left);
-            PaddleMouv_right(ctx, canvas, paddleProps_right);
-          } else {
-            draw_score(
-              ctx,
-              player_left,
-              player_right,
-              canvas.height,
-              canvas.width
-            );
-          }
-          requestAnimationFrame(render);
-        }
-      };
-      render();
-    });
-  }, [socket]);
+  The_whole_game(canvasRef);
 
   if (gamestart == true && im_right == true) {
     return (
       <div>
-        <GameJoined_left setRoom={setRoom} canvasRef={canvasRef} 
-        mouv_paddle_right={mouv_paddle_right} deleteGameRoom={deleteGameRoom} 
+        <GameStarted_left setRoom={setRoom} canvasRef={canvasRef}
+        deleteGameRoom={deleteGameRoom} gamestart={gamestart} im_right={im_right}
         my_id={my_id} op_id={op_id} room={room}/>
       </div>
     );
   } else if (gamestart == true && im_right == false) {
     return (
       <div>
-        <GameJoined_right setRoom={setRoom} canvasRef={canvasRef} 
-        mouv_paddle_left={mouv_paddle_left} deleteGameRoom={deleteGameRoom} 
+        <GameStarted_right setRoom={setRoom} canvasRef={canvasRef} 
+        deleteGameRoom={deleteGameRoom} gamestart={gamestart} im_right={im_right}
         my_id={my_id} op_id={op_id} room={room}/>
       </div>
     );
