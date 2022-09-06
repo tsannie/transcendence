cddaee6b51991d0c88936fe719e92153d6afd463
dodjian <dovdjianpro@gmail.com>
2022-09-06 11:47:35 +0200

    [fix] fix cookies and state for login/logout

--- srcs/front_react/src/components/chat/channels/ChannelsList.tsx
+++ srcs/front_react/src/components/chat/channels/ChannelsList.tsx
@@ -1,48 +1,43 @@
 import { Box, Grid } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { api } from "../../../userlist/UserListItem";
-import { socket } from "../Chat";
 import { IChannel } from "../types";
 
 // to do: channel list
 // faire un call api to channel/all pour afficher les channels
 
-export default function ChannelsList() {
+async function DisplayChannels() {
+    await api.get('channel/all').then(res => {
+      console.log(res.data);
+      //setId(res.data.id);
+      //setUsername(res.data.username);
+      //setEmail(res.data.email);
+    }).catch(res => {
+      console.log('invalid channels');
+      console.log(res)
+    });
+}
 
-  const [channelsList, setChannelsList] = useState<Array<IChannel>>([]);
-
-  // get all channels
-  function getChannels() {
-    api
-      .get("channel/all")
-      .then((res) => {
-        setChannelsList(res.data);
-      })
-      .catch((res) => {
-        console.log("invalid channels");
-        console.log(res);
-      });
-  }
-
-  // call getChannels() when channelList change
-  useEffect(() => {
-    getChannels();
-  }, [channelsList]);
-
-  return (
-    <Box>
-      {channelsList.map((channelData: IChannel) => {
-        return (
-          <Box
-            sx={{
-              color: "red",
-            }}
-            key={channelData.name}
-          >
-            {channelData.name}
-          </Box>
-        );
-      })}
-    </Box>
-  );
+export default function ChannelsList(props: any) {
+  if (props.channelCreated === true)
+    return (
+      <Box
+        sx={{
+          width: "fit-content",
+          height: "fit-content",
+          backgroundColor: "#064fbd",
+          color: "white",
+          fontFamily: "sans-serif",
+          fontSize: 16,
+          borderRadius: 12,
+          ml: "auto",
+          mr: 0.5,
+          mb: 1,
+          p: 1,
+        }}
+        key={props.channelData.id}
+      >
+        {props.channelData.content}
+      </Box>
+    );
 }
