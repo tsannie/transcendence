874dd6342cc3fdc8925d8dbd6d646b401b8468d3
dodjian <dovdjianpro@gmail.com>
2022-09-06 17:02:33 +0200

    [+] start channelsList

--- srcs/front_react/src/components/chat/channels/ChannelsList.tsx
+++ srcs/front_react/src/components/chat/channels/ChannelsList.tsx
@@ -1,43 +1,61 @@
 import { Box, Grid } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { api } from "../../../userlist/UserListItem";
+import { socket } from "../Chat";
 import { IChannel } from "../types";
 
 // to do: channel list
 // faire un call api to channel/all pour afficher les channels
 
-async function DisplayChannels() {
-    await api.get('channel/all').then(res => {
-      console.log(res.data);
-      //setId(res.data.id);
-      //setUsername(res.data.username);
-      //setEmail(res.data.email);
-    }).catch(res => {
-      console.log('invalid channels');
-      console.log(res)
-    });
-}
-
 export default function ChannelsList(props: any) {
-  if (props.channelCreated === true)
-    return (
-      <Box
-        sx={{
-          width: "fit-content",
-          height: "fit-content",
-          backgroundColor: "#064fbd",
-          color: "white",
-          fontFamily: "sans-serif",
-          fontSize: 16,
-          borderRadius: 12,
-          ml: "auto",
-          mr: 0.5,
-          mb: 1,
-          p: 1,
-        }}
-        key={props.channelData.id}
-      >
-        {props.channelData.content}
-      </Box>
-    );
+
+  let i = 0;
+  const [channelsList, setChannelsList] = useState<Array<IChannel>>([]);
+
+  console.log("danschannellist")
+
+  function getChannels() {
+    api
+      .get("channel/all")
+      .then((res) => {
+        console.log(res.data);
+        setChannelsList(res.data);
+      })
+      .catch((res) => {
+        console.log("invalid channels");
+        console.log(res);
+      });
+  }
+
+  console.log(props.channelCreated);
+
+  useEffect(() => {
+    const strChannelsList = JSON.parse(window.localStorage.getItem("channelsList") || "null");
+    setChannelsList(strChannelsList);
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("channelsList", JSON.stringify(channelsList));
+  }, [channelsList]);
+
+  useEffect(() => {
+    getChannels();
+  }, [channelsList]);
+
+  return (
+    <Box>
+      {channelsList.map((channelData: IChannel) => {
+        return (
+          <Box
+            sx={{
+              color: "red",
+            }}
+            key={channelData.id}
+          >
+            {channelData.id}
+          </Box>
+        );
+      })}
+    </Box>
+  );
 }
