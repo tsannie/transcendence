0e1382d779ecb7cf8f9db8bb675055507e93b8df
philippebarradas <philippe.lb.barradas@gmail.com>
2022-09-07 16:47:57 +0200

    [SAVE] div all

--- srcs/front_react/src/game/GameSpectator.tsx
+++ srcs/front_react/src/game/GameSpectator.tsx
@@ -1,25 +1,67 @@
+import { useEffect, useState } from "react";
+import { socket } from "./Game";
 
 
 
+export  function The_whole_spectator(store: any) {
+
+  useEffect(() => {
+
+    socket.on("getAllGameRoom", (theroom: Map<any, any>) => {
+      console.log("getAllGameRoom watch client side");
+
+      console.log("1 socker");
+
+      for (const [key, value] of Object.entries(theroom)) {
+        console.log("rooma are : [" + key + "][" + "]");
+      }
+
+      console.log("2 socket");
+    });
+
+  }, [socket]);
+
+}
+
+export function GameSpectator(props: any) {
+
+  function lookthegame() {
+    //setisLookingRoom(false);
+    
+    console.log(
+      "lookthegamelookthegamelookthegamelookthegamelookthegame lookthegame"
+    );
+    //console.log(listGame)
+
+    console.log("/*/**//*/**/   " + props.store.lookingroom);
+    socket.emit("lookGameRoom", props.store.lookingroom);
+  }
+
+  function leavelookingroom() {
+    props.store.setisLookingRoom(false);
+    props.store.setLookingRoom("");
+    console.log("list when living '" + props.store.listGame);
+    props.store.setListGame([]);
+    console.log("list when living then '" + props.store.listGame);
+  }
 
 
-export default function GameSpectator(props: any) {
   return (
     <div className="look">
       <h4> REGARDER une partie : </h4>
       <p> wich game do you want to look at ?</p>
-      {props.listGame.map((element: any, index: any) => {
-        props.setLookingRoom(element);
+      {props.store.listGame.map((element: any, index: any) => {
+        props.store.setLookingRoom(element);
         return (
           <div key={index}>
             <p>
               partie : "{element}"{" "}
-              <button onClick={props.lookthegame}>regarder la partie</button>
+              <button onClick={lookthegame}>regarder la partie</button>
             </p>
           </div>
         );
       })}
-      <button onClick={props.leavelookingroom}>leave</button>
+      <button onClick={leavelookingroom}>leave</button>
     </div>
   );
 }
