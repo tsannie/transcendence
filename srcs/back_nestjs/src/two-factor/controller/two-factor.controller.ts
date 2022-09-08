f75f278a28779052c0c252c2ac22985a4d65412c
tsannie <tsannie@student.42.fr>
2022-09-08 15:14:01 +0200

    [+] check valide token

--- srcs/back_nestjs/src/two-factor/controller/two-factor.controller.ts
+++ srcs/back_nestjs/src/two-factor/controller/two-factor.controller.ts
@@ -1,16 +1,19 @@
-import { Body, Controller, Get, Post, Req, Request, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post, Req, Request, Res, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Response } from 'express';
 import { UserService } from 'src/user/service/user.service';
+import { logger2FA } from '../const/const';
+import { TokenDto } from '../dto/token.dto';
 import { TwoFactorService } from '../service/two-factor.service';
 
 @Controller('2fa')
 export class TwoFactorController {
   constructor(
-    private readonly twoFactorService: TwoFactorService
+    private readonly twoFactorService: TwoFactorService,
     private readonly userService: UserService
   ) {}
 
+  // generate a new qrcode for the user
   @UseGuards(AuthGuard('jwt'))
   @Get('generate')
   async generate(@Res() response: Response, @Request() req) {
@@ -21,17 +24,17 @@ export class TwoFactorController {
     return this.twoFactorService.generateQrCode(response, otpauthUrl)
   }
 
+  // verify the token and enable 2FA for the user
   @UseGuards(AuthGuard('jwt'))
   @Post('check-token')
-  async verifyToken(@Body() token: string, @Request() req) {
-    const valid =  await this.twoFactorService.codeIsValid(token, req.user);
+  async verifyToken(@Body() tokenBody: TokenDto, @Request() req) {
+    const valid =  await this.twoFactorService.codeIsValid(tokenBody.token, req.user);
 
     if (valid) {
       await this.userService.enable2FA(req.user.id)
+      logger2FA.log(`2FA enabled for user ${req.user.username}`)
     } else {
-      throw new Error('Invalid token')
+      throw new UnauthorizedException('Invalid token')
     }
   }
-
-
 }
