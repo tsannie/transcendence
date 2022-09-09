9684557059127ba97fbc1340fabf8b6df6ff0906
Guillaume PETIT <gpetit@e2r9p8.clusters.42paris.fr>
2022-09-09 16:45:28 +0200

    [+] added password in dto and password and salt in corresponding entity

--- srcs/back_nestjs/src/channel/dto/channel.dto.ts
+++ srcs/back_nestjs/src/channel/dto/channel.dto.ts
@@ -1,13 +1,26 @@
-import { IsDefined, IsNotEmpty } from "class-validator";
+import { Contains, Equals, IsAlphanumeric, IsDefined, IsIn, IsNotEmpty, IsOptional, IsString, Max, MaxLength, Min, MinLength } from "class-validator";
 import { CreateDateColumn } from "typeorm";
+
+const statuses = [ "Public", "Private", "Protected" ]
 
 export class ChannelDto {
 
   @IsDefined()
   @IsNotEmpty()
+  @IsString()
   name: string;
 
   @IsDefined()
   @IsNotEmpty()
+  @IsString()
+  @IsIn(statuses)
   status: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  @IsAlphanumeric()
+  @MinLength(1)
+  @MaxLength(10)
+  password: string;
 }
