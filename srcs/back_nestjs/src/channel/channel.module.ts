906955fb09177a895b6d65128f9dcbe14ba35b2f
Guillaume PETIT <gpetit@e1r1p8.clusters.42paris.fr>
2022-09-15 09:37:25 +0200

    [fix] added modules used in message entity in typeorm.forFeature message module

--- srcs/back_nestjs/src/channel/channel.module.ts
+++ srcs/back_nestjs/src/channel/channel.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MessageEntity } from 'src/message/models/message.entity';
 import { UserEntity } from 'src/user/models/user.entity';
 import { ChannelController } from './controller/channel.controller';
 import { ChannelEntity } from './models/channel.entity';
@@ -7,8 +8,7 @@ import { ChannelService } from './service/channel.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ChannelEntity]),
-    TypeOrmModule.forFeature([UserEntity])
+    TypeOrmModule.forFeature([ChannelEntity, UserEntity, MessageEntity]),
   ],
   controllers: [ChannelController],
   providers: [ChannelService],
