46c35e4f201798ae8a4d8cefad8ae57383d278ea
Dov Djian <dovdjianpro@gmail.com>
2022-09-15 17:17:41 +0200

    [+] start dm back, fix relation name pbm

--- srcs/back_nestjs/src/dm/controller/dm.controller.ts
+++ srcs/back_nestjs/src/dm/controller/dm.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { DmEntity } from '../models/dm.entity';
 
 @Controller('dm')
-export class DmController {}
+export class DmController {
+  /* @UseGuards( AuthGuard('jwt') )
+  @Post('createDm')
+  async createDm(@Body() channel: CreateDmDto, @Request() req): Promise<void | DmEntity> {
+    return await this.dmService.createDm(channel, req.user);
+  } */
+}
--- srcs/back_nestjs/src/dm/service/dm.service.ts
+++ srcs/back_nestjs/src/dm/service/dm.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, UnprocessableEntityException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/models/user.entity';
+import { Repository } from 'typeorm';
+import { DmEntity } from '../models/dm.entity';
 
 @Injectable()
-export class DmService {}
+export class DmService {
+  constructor(
+    @InjectRepository(DmEntity)
+    private dmRepository: Repository<DmEntity>,
+  ) {}
+
+  // save mp in db with unprocessable exception if error
+  async saveDm(dm: DmEntity): Promise<void | DmEntity> {
+    try {
+      return await this.dmRepository.save(dm);
+    } catch (e) {
+      if (e.code === '23505') {
+        throw new UnprocessableEntityException('Name of Server already exist. Choose another one.');
+      } else {
+        console.log(e);
+        throw new InternalServerErrorException('Saving of new Channel failed.');
+      }
+    }
+  }
+
+
+  // create a new dm
+  /* async createDm(channel: CreateDmDto, user: UserEntity): Promise<void | DmEntity> {
+    let dm = new DmEntity();
+
+    dm.name = channel.name;
+    dm.users = [user];
+    dm.messages = [];
+    return await dm.save();
+  } */
+}
