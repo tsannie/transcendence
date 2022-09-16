f80309a4c4904fef35cddc741a11ef6207230997
Dov Djian <dovdjianpro@gmail.com>
2022-09-16 17:49:27 +0200

    [fix] change icon sidebar. to do: svg iconlogo

--- srcs/front_react/src/components/sidebar/Sidebar.tsx
+++ srcs/front_react/src/components/sidebar/Sidebar.tsx
@@ -2,16 +2,21 @@ import { useState } from "react";
 import Chat from "../chat/Chat";
 import LogoIcon from "../../assets/logo-project.png";
 //import HomeIcon from "../../assets/home.png";
-import ProfileIcon from "../../assets/profile.png";
-import GameIcon from "../../assets/game.png";
+//import ProfileIcon from "../../assets/profile.png";
+//import GameIcon from "../../assets/game.png";
 import ChatIcon from "../../assets/chat.png";
 //import SettingsIcon from "../../assets/settings.png";
 import ExitIcon from "../../assets/exit.png";
 import { Box } from "@mui/system";
-import { Grid, IconButton } from "@mui/material";
+import { createSvgIcon, Grid, IconButton, SvgIcon } from "@mui/material";
 import { COOKIE_NAME } from "../../const";
 import { Alarm, Settings } from "@material-ui/icons";
 import HomeIcon from '@mui/icons-material/Home';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LogoutIcon from '@mui/icons-material/Logout';
+import MessageIcon from '@mui/icons-material/Message';
+//import {ReactComponent as LogoIcon} from "../../assets/logo.svg";
 
 export default function Sidebar(props: any) {
   //const [displayGame, setDisplayGame] = useState(false);
@@ -46,13 +51,20 @@ export default function Sidebar(props: any) {
         </IconButton>
       </Grid>
       <Grid item sx={{}}>
-        <img src={ProfileIcon}></img>
+        <IconButton aria-label="profile">
+          <AccountCircleIcon />
+        </IconButton>
       </Grid>
       <Grid item sx={{}}>
-        <img src={GameIcon}></img>
+        <IconButton aria-label="game">
+          <SportsEsportsIcon />
+        </IconButton>
       </Grid>
       <Grid item sx={{}}>
-        <img src={ChatIcon} onClick={() => props.setInputChat(true)}></img>
+      <IconButton aria-label="logout"
+         onClick={() => props.setInputChat(true)}>
+          <MessageIcon />
+        </IconButton>
       </Grid>
       <Grid item sx={{}}>
         <IconButton aria-label="settings">
@@ -60,7 +72,10 @@ export default function Sidebar(props: any) {
         </IconButton>
       </Grid>
       <Grid item sx={{}}>
-        <img src={ExitIcon} onClick={logout}></img>
+        <IconButton aria-label="logout"
+          onClick={() => logout()}>
+          <LogoutIcon />
+        </IconButton>
       </Grid>
     </Grid>
   );
