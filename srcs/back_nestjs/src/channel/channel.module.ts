1801641968d1eb118e148d5de5956ac2e40e76ab
gpetit <gpetit@student.42.fr>
2022-09-18 20:34:32 +0200

    [+] changed banmuteDto to channelactionsDto and added ban and unban functions

--- srcs/back_nestjs/src/channel/channel.module.ts
+++ srcs/back_nestjs/src/channel/channel.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MessageEntity } from 'src/message/models/message.entity';
 import { UserEntity } from 'src/user/models/user.entity';
+import { UserService } from 'src/user/service/user.service';
 import { ChannelController } from './controller/channel.controller';
 import { ChannelEntity } from './models/channel.entity';
 import { ChannelService } from './service/channel.service';
@@ -11,7 +12,7 @@ import { ChannelService } from './service/channel.service';
     TypeOrmModule.forFeature([ChannelEntity, UserEntity, MessageEntity]),
   ],
   controllers: [ChannelController],
-  providers: [ChannelService],
+  providers: [ChannelService, UserService],
   exports: [ChannelService]
 })
 export class ChannelModule {}
