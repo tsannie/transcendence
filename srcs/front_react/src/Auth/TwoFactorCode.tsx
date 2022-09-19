4084f35656115dfd61240c524ae2262bfbc8302c
tsannie <tsannie@student.42.fr>
2022-09-19 17:20:46 +0200

    [+] react code input

--- srcs/front_react/src/Auth/TwoFactorCode.tsx
+++ srcs/front_react/src/Auth/TwoFactorCode.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import ReactCodeInput from "react-verification-code-input";
+import './twofactor.style.scss'
 
 export default function TwoFactorCode() {
+  const [token, setToken] = React.useState("");
+
+  const handleOnChange = (up: string) => {
+    setToken(up);
+    console.log(up);
+  };
+
+  const handleOnComplete = () => {
+
+    console.log("complete");
+  };
+
   return (
     <div>
       <h1>Validation Code</h1>
+      <ReactCodeInput
+        className='authInput'
+        onChange={handleOnChange}
+        onComplete={handleOnComplete}
+      />
     </div>
   );
 }
