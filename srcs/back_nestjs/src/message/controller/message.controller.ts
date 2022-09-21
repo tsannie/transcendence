40229b6ab1aaf9d8dc5200fc15b7655b62c5ae06
Guillaume PETIT <gpetit@e1r3p18.clusters.42paris.fr>
2022-09-21 18:12:51 +0200

    [ ] cleaned module import/export in channel and others modules. Code doesn't compile if userService is deleted.

--- srcs/back_nestjs/src/message/controller/message.controller.ts
+++ srcs/back_nestjs/src/message/controller/message.controller.ts
@@ -1,12 +1,4 @@
-import { Body, Controller, Get, Header, Post } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { MessageBody } from '@nestjs/websockets';
-import { from, Observable } from 'rxjs';
-import { Socket } from 'socket.io';
-import { UserService } from 'src/user/service/user.service';
-import { Repository } from 'typeorm';
-import { MessageGateway } from '../message.gateway';
-import { MessageEntity } from '../models/message.entity';
+import { Controller } from '@nestjs/common';
 import { MessageService } from '../service/message.service';
 
 @Controller('message')
