12c5d75359e7794e30aca3914927a8d3674954bb
Guillaume PETIT <gpetit@e1r2p16.clusters.42paris.fr>
2022-09-22 15:13:27 +0200

    [fix] changed dm entity to enable cascade on insert/remove and deleted onDelete: CASCADE in messageEntity

--- srcs/back_nestjs/src/dm/dto/dm.dto.ts
+++ srcs/back_nestjs/src/dm/dto/dm.dto.ts
@@ -1,10 +1,8 @@
 import { IsDefined, IsNotEmpty, IsString } from "class-validator";
-import { MessageEntity } from "src/message/models/message.entity";
-import { UserEntity } from "src/user/models/user.entity";
 
 export class CreateDmDto {
   @IsDefined()
   @IsNotEmpty()
   @IsString()
-  targetUsername: string;
+  target: string;
 }
