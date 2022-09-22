ba2dab5fb8bdae612b599b7cc1a5c15a48d24755
tsannie <tsannie@student.42.fr>
2022-09-22 03:18:29 +0200

    [+] start ball effect lobby

--- srcs/front_react/src/App.tsx
+++ srcs/front_react/src/App.tsx
@@ -43,16 +43,29 @@ export default function App() {
     }
   });
 
+  const test = "--i:10;";
+
   return (
     <div className="app">
-      {isLogin === false &&
-        <LoginPage setIsLogin={setIsLogin}
-          is2FA={is2FA}
-          isLogin={isLogin}
-        />
-      }
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
     </div>
   );
+
+  /*{isLogin === false &&
+    <LoginPage setIsLogin={setIsLogin}
+      is2FA={is2FA}
+      isLogin={isLogin}
+    />
+  }*/
 
   /*if (!isLogin) {
     return (
