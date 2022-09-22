57fea957a9513e9584428f2eb6b069c288e19156
tsannie <tsannie@student.42.fr>
2022-09-22 22:56:28 +0200

    [+] start first page

--- srcs/front_react/src/components/auth/ButtonLogin.tsx
+++ srcs/front_react/src/components/auth/ButtonLogin.tsx
@@ -1,16 +1,23 @@
-import { Box } from "@mui/system";
 import { REDIRECT_LINK_AUTH } from "../../const/const";
+import './login.style.scss'
+import FTLogo from "../../assets/42_Logo.svg";
 
 export default function ButtonLogin(props: any) {
 
   function linkLog(event: any) {
-    //event.preventDefault();
+    event.preventDefault();
     window.location.href = REDIRECT_LINK_AUTH;
   }
 
+
+  //
+
   return (
+    <div className='listConnection'>
       <button onClick={linkLog}>
-        Login
+        <img src={FTLogo}/>
+        <p>Continue with 42</p>
       </button>
+    </div>
   );
 }
