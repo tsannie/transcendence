dec7d7da4a8355fad7c4cb08f2f6add23ca1643e
tsannie <tsannie@student.42.fr>
2022-09-26 17:27:31 +0200

    [+] start sidebar

--- srcs/front_react/src/components/sidebar/Sidebar.tsx
+++ srcs/front_react/src/components/sidebar/Sidebar.tsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
+import HomeIcon from "../../assets/img/icon/home.png";
+import ChatIcon from "../../assets/img/icon/chat.png";
 import Chat from "../chat/Chat";
 import LogoIcon from "../../assets/logo-project.png";
-import HomeIcon from "../../assets/home.png";
 import ProfileIcon from "../../assets/profile.png";
 import GameIcon from "../../assets/game.png";
-import ChatIcon from "../../assets/chat.png";
 import SettingsIcon from "../../assets/settings.png";
 import ExitIcon from "../../assets/exit.png";
 import { Box } from "@mui/system";
 import { Grid } from "@mui/material";
 import { COOKIE_NAME } from "../../const/const";
+import './sidebar.style.scss';
 
 export default function Sidebar(props: any) {
   //const [displayGame, setDisplayGame] = useState(false);
@@ -18,62 +19,19 @@ export default function Sidebar(props: any) {
 
   // to do: passer d'icone en icone en remettant tous les autres state a false
 
-  function logout() { // TODO route api logout
-    document.cookie = COOKIE_NAME + '=; Max-Age=-1;;';
-    props.setIsLogin(false);
-    props.setIs2FA(false);
-    window.location.reload();
-  };
 
-  function resetInput() {
-    props.setInputChat(false);
-    props.setInputSettings(false);
-  }
 
-  function selectInput(propsName: string) {
-    resetInput();
-    switch (propsName) {
-      case "Chat":
-        props.setInputChat(true);
-        break;
-      case "Settings":
-        props.setInputSettings(true);
-        break;
-    }
-  }
 
   return (
-    <Grid
-      spacing={10}
-      alignItems="center"
-      container
-      direction="column"
-      sx={{
-        maxWidth: "fit-content",
-        maxHeight: "fit-content",
-      }}
-    >
-      <Grid item sx={{}}>
-        <img src={LogoIcon}></img>
-      </Grid>
-      <Grid item sx={{}}>
-        <img src={HomeIcon}></img>
-      </Grid>
-      <Grid item sx={{}}>
-        <img src={ProfileIcon}></img>
-      </Grid>
-      <Grid item sx={{}}>
-        <img src={GameIcon}></img>
-      </Grid>
-      <Grid item sx={{}}>
-        <img src={ChatIcon} onClick={() => selectInput('Chat')}></img>
-      </Grid>
-      <Grid item sx={{}}>
-        <img src={SettingsIcon} onClick={() => selectInput('Settings')}></img>
-      </Grid>
-      <Grid item sx={{}}>
-        <img src={ExitIcon} onClick={logout}></img>
-      </Grid>
-    </Grid>
+    <div className="sidebar">
+      <img src={HomeIcon}></img>
+      <img src={ChatIcon}></img>
+      <img src={LogoIcon}></img>
+      <img src={ProfileIcon}></img>
+      <img src={GameIcon}></img>
+      <img src={SettingsIcon}></img>
+      <img src={ExitIcon}></img>
+    </div>
+
   );
 }
