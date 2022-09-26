d15d0071a09658c4e43812dc5af6eb4b16b0e536
tsannie <tsannie@student.42.fr>
2022-09-26 11:57:29 +0200

    [fix] responsive font

--- srcs/front_react/src/components/auth/LoginPage.tsx
+++ srcs/front_react/src/components/auth/LoginPage.tsx
@@ -2,7 +2,6 @@ import { Box } from "@mui/material";
 import React, { useEffect } from "react";
 import ButtonLogin from "./ButtonLogin";
 import TwoFactorCode from "./TwoFactorCode";
-import LogoIcon from "../../assets/logo-project.png";
 import './login.style.scss'
 
 
@@ -14,10 +13,10 @@ export default function LoginPage(props: any) {
       <h1>transcendence</h1>
       <div className="loginBox">
         <h2>Account Sign in</h2>
-        {!props.is2FA &&
+        { !props.is2FA &&
           <ButtonLogin isLogin={props.isLogin} setIsLogin={props.setIsLogin} />
         }
-        {props.is2FA &&
+        { props.is2FA &&
           <TwoFactorCode setIsLogin={props.setIsLogin}/>
         }
       </div>
