e3a7b0673709bfa5d5254e899b5b111292623a4e
tsannie <tsannie@student.42.fr>
2022-09-27 18:30:36 +0200

    [fix] selection

--- srcs/front_react/src/components/menu/Home.tsx
+++ srcs/front_react/src/components/menu/Home.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../sidebar/Sidebar";
 import './home.style.scss';
 
-export default function Home() {
+export default function Home() { // TODO replace home by menu
+  const [selected, setSelected] = useState('home');
+
   return (
     <div className="home">
-      <Sidebar/>
-      <h1>Menu</h1>
+      <Sidebar setSelected={setSelected} selected={selected}/>
+      { selected == 'home' &&
+        <h1>home</h1>
+      }
+      { selected == 'profile' &&
+        <h1>profile</h1>
+      }
+      { selected == 'chat' &&
+        <h1>chat</h1>
+      }
+      { selected == 'game' &&
+        <h1>game</h1>
+      }
+      { selected == 'settings' &&
+        <h1>settings</h1>
+      }
+      { selected == 'logout' &&
+        <h1>logout</h1>
+      }
     </div>
   );
 }
