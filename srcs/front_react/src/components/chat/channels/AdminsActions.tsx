db8caf66a290d14da33e7912fb18c3737481251c
dodjian <dovdjianpro@gmail.com>
2022-09-27 16:11:17 +0200

    [fix] change secret and start makeAdmin

--- srcs/front_react/src/components/chat/channels/AdminsActions.tsx
+++ srcs/front_react/src/components/chat/channels/AdminsActions.tsx
@@ -1,56 +1,58 @@
-import { Button, List, ListItemButton, Popover } from "@mui/material";
+import { Button, List, ListItem, ListItemButton, Popover } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { api } from "../../../userlist/UserListItem";
 import { IChannel } from "../types";
 import BanUser from "./BanUser";
+import MakeAdmin from "./MakeAdmin";
 import MuteUser from "./MuteUser";
+import RevokeAdmin from "./RevokeAdmin";
 import UnbanUser from "./UnbanUser";
 import UnmuteUser from "./UnmuteUser";
 
 export default function AdminsActions(props: any) {
   const [infosChannel, setInfosChannel] = useState<IChannel>();
 
-  /* function isMuted(channel: any) {
-    console.log("channel = ", channel);
-    //console.log("channel.muted.length = ", channel.muted.length);
-    // parcourir les muted users et return true si userId === muted.id
+  // handleAdminActionsClick function to open popover
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
+  function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
+  function isAdmin(channel: any) {
     if (channel !== undefined) {
-      for (let i = 0; i < channel.muted.length; i++) {
-        if (channel.muted[i].id === props.userId) {
+      for (let i = 0; i < channel.admins.length; i++) {
+        if (channel.admins[i].id === props.userId) {
           return true;
         }
       }
     }
-    console.log("isMuted = false");
     return false;
   }
 
-  function isBanned(channel: any) {
-    if (channel !== undefined) {
-      for (let i = 0; i < channel.banned.length; i++) {
-        if (channel.banned[i].id === props.userId) {
-          return true;
-        }
-      }
-    }
-    return false;
-  } */
-
   async function getInfosChannel(channel: IChannel) {
-    await api
-      .get("channel/privateData", {
-        params: {
-          name: channel.name,
-        },
-      })
-      .then((res) => {
-        console.log("get infos channels");
-        setInfosChannel(res.data);
-        //setChannelId(res.data.id);
-      })
-      .catch((res) => {
-        console.log("invalid channels private data");
-      });
+    if (props.isOwner(channel)) {
+      await api
+        .get("channel/privateData", {
+          params: {
+            name: channel.name,
+          },
+        })
+        .then((res) => {
+          console.log("get infos channels");
+          setInfosChannel(res.data);
+          //setChannelId(res.data.id);
+        })
+        .catch((res) => {
+          console.log("invalid channels private data");
+        });
+    }
   }
 
   useEffect(() => {
@@ -61,10 +63,54 @@ export default function AdminsActions(props: any) {
 
   return (
     <>
-      <MuteUser channelData={props.channelData} />
-      <UnmuteUser channelData={props.channelData} />
-      <BanUser channelData={props.channelData} />
-      <UnbanUser channelData={props.channelData} />
+      {isAdmin(infosChannel) || props.isOwner(props.channelData) ? (
+        <div>
+          <Button
+            //aria-describedby={id}
+            //variant="contained"
+            onClick={handleClick}
+          >
+            Admin Actions
+          </Button>
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            <List>
+              <ListItem>
+                <BanUser channelData={props.channelData} />
+              </ListItem>
+              <ListItem>
+                <UnbanUser channelData={props.channelData} />
+              </ListItem>
+              <ListItem>
+                <MuteUser channelData={props.channelData} />
+              </ListItem>
+              <ListItem>
+                <UnmuteUser channelData={props.channelData} />
+              </ListItem>
+              <ListItem>
+                <MakeAdmin channelData={props.channelData} />
+              </ListItem>
+              <ListItem>
+                <RevokeAdmin channelData={props.channelData} />
+              </ListItem>
+            </List>
+          </Popover>
+        </div>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
