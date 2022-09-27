0060fa6e75998f9a4601bd7d2039dc2d013e4e02
gpetit <gpetit@student.42.fr>
2022-09-27 23:15:51 +0200

    [+] function to add message to channel and to add message to DM coded. Patched entity from a One to One to a Many to One relationship

--- srcs/back_nestjs/src/message/controller/message.controller.ts
+++ srcs/back_nestjs/src/message/controller/message.controller.ts
@@ -1,8 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { MessageService } from '../service/message.service';
 
 @Controller('message')
 export class MessageController {
   constructor(private messageService: MessageService,
     ) {}
+
+
+	//DELETE THIS TEST ROUTE
+	@Post("addChannel")
+	async addMessageChannel(@Body() data) {
+		return await this.messageService.addMessagetoChannel(data);
+	}
+
+	@Post("addDm")
+	async addDm(@Body() data) {
+		return await this.messageService.addMessagetoDm(data);
+	}
 }
