1a67f7b67cf9e28cb825568f69943fdc296a0e00
Guillaume PETIT <gpetit@e1r5p9.clusters.42paris.fr>
2022-10-04 17:57:11 +0200

    [+] loading dm and all messages. Next step would implement offset and loading messages by 20

--- srcs/back_nestjs/src/dm/dto/dm.dto.ts
+++ srcs/back_nestjs/src/dm/dto/dm.dto.ts
@@ -1,8 +1,23 @@
-import { IsDefined, IsNotEmpty, IsString } from "class-validator";
+import { Type } from "class-transformer";
+import { IsDefined, IsDivisibleBy, IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString, Min } from "class-validator";
 
 export class DmDto {
-  @IsDefined()
+  @IsOptional()
   @IsNotEmpty()
   @IsString()
   target: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @Type( () => Number )
+  @IsNumber()
+  id: number;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @Type( () => Number )
+  @IsNumber()
+  @Min(0)
+  @IsDivisibleBy(20)
+  offset: number
 }
