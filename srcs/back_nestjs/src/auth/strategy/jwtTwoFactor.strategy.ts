114084bc5a3320dfefb8f25d63a63033f38318f4
dodjian <dovdjianpro@gmail.com>
2022-10-06 01:35:53 +0200

    [+] add delete button. TODO: add it when i get

--- srcs/back_nestjs/src/auth/strategy/jwtTwoFactor.strategy.ts
+++ srcs/back_nestjs/src/auth/strategy/jwtTwoFactor.strategy.ts
@@ -21,7 +21,13 @@ export class JwtTwoFactorStrategy extends PassportStrategy(Strategy, 'jwt-2fa') 
   }
 
   async validate(payload: IPayload) {
-    const user = await this.userService.findById(payload.sub);
+    const user = await this.userService.findById(payload.sub, {
+      owner_of: true,
+      admin_of: true,
+      channels: true,
+      banned: true,
+      dms: true,
+      });
     if (!user.enabled2FA) {
       return user;
     }
