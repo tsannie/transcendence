cb866629f92645b3c4947967062df78f755def4b
dodjian <dovdjianpro@gmail.com>
2022-10-07 12:11:03 +0200

    [fix] TODO: continue admins actions

--- srcs/front_react/src/components/chat/channels/InfosChannels.tsx
+++ srcs/front_react/src/components/chat/channels/InfosChannels.tsx
@@ -12,6 +12,7 @@ import { api } from "../../../const/const";
 import { IChannel } from "../types";
 
 interface InfosChannelsProps {
+  username: string;
   channelData: any;
 }
 
@@ -20,41 +21,50 @@ export default function InfosChannels(props: InfosChannelsProps) {
 
   function handleClick() {
     console.log("click on user who i wnat to ban, mute etc");
+    console.log("channel", props.channelData);
   }
 
   return (
     <Grid container>
       <Grid item>
         <List>
+          Owner
           <ListItem>
-            <ListItemButton key={props.channelData.name}
+            <ListItemButton key={props.channelData.data.name}
               onClick={handleClick}
             >
-              <ListItemText>{props.channelData.owner.username}</ListItemText>
+             {<ListItemText>{props.channelData.data.owner.username}</ListItemText> }
             </ListItemButton>
           </ListItem>
         </List>
       </Grid>
 
-      {/* <Grid item>
+      <Grid item>
         <List>
-          {props.channelData.data.admins.map((user) => (
-            <ListItem>
+          Admins
+          {props.channelData.data.admins.map((user: any) => (
+            <ListItemButton key={user.username}
+              onClick={handleClick}
+            >
               <ListItemText primary={user.username} />
-            </ListItem>
+            </ListItemButton>
           ))}
         </List>
       </Grid>
 
       <Grid item>
         <List>
-          {props.channelData.data.users.map((user) => (
-            <ListItem>
+          Users
+          {props.channelData.data.users.map((user: any) => (
+            <ListItemButton key={user.username}
+              onClick={handleClick}
+              disabled={props.username !== props.channelData.data.owner.username}
+            >
               <ListItemText primary={user.username} />
-            </ListItem>
+            </ListItemButton>
           ))}
         </List>
-      </Grid> */}
+      </Grid>
     </Grid>
   );
 }
