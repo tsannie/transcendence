11bb6e11512bb26f5e718f2426d27b8e078838d0
tsannie <tsannie@student.42.fr>
2022-10-07 15:36:47 +0200

    [fix] sidebar

--- srcs/front_react/src/components/sidebar/Sidebar.tsx
+++ srcs/front_react/src/components/sidebar/Sidebar.tsx
@@ -7,6 +7,7 @@ import LogOutIcon from "../../assets/img/icon/log-out.png";
 import ProfileIcon from "../../assets/img/icon/user.png";
 import { COOKIE_NAME } from "../../const/const";
 import "./sidebar.style.scss";
+import { Link } from "react-router-dom";
 
 export default function Sidebar(props: any) {
   //const [displayGame, setDisplayGame] = useState(false);
@@ -17,16 +18,26 @@ export default function Sidebar(props: any) {
 
   return (
     <div className="sidebar">
-      <div className="sidebar__icon">
-        <nav>
+      <nav className="sidebar__icon">
+        <Link to="/">
           <img src={HomeIcon}></img>
+        </Link>
+        <Link to="/profile">
           <img src={ProfileIcon}></img>
+        </Link>
+        <Link to="/chat">
           <img src={ChatIcon}></img>
+        </Link>
+        <Link to="/game">
           <img src={GameIcon}></img>
+        </Link>
+        <Link to="/settings">
           <img src={SettingsIcon}></img>
+        </Link>
+        <Link to="/logout">
           <img src={LogOutIcon}></img>
-        </nav>
-      </div>
+        </Link>
+      </nav>
     </div>
   );
 }
