ef781f977ed7866309ff6e697612a97e40e841fb
tsannie <tsannie@student.42.fr>
2022-10-11 16:26:58 +0200

    [fix] snackbar

--- srcs/back_nestjs/src/user/controller/user.controller.ts
+++ srcs/back_nestjs/src/user/controller/user.controller.ts
@@ -1,14 +1,24 @@
-import { Body, Controller, Delete, Get, Header, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Header, Post, Request, UseGuards } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { UserService } from '../service/user.service';
 import { UserEntity } from '../models/user.entity';
+import JwtTwoFactorGuard from 'src/auth/guard/jwtTwoFactor.guard';
+import { UpdateResult } from 'typeorm';
+import { NewUsernameDto } from '../models/newusername.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  // edit username
+  @Post('edit-username')
+  @UseGuards(JwtTwoFactorGuard)
+  async editUsername(@Request() req, @Body() newUsername: NewUsernameDto): Promise<UpdateResult> {
+    return await this.userService.editUsername(req.user.id, newUsername.username);
+  }
+
   @Get()
-  async getAllUser():  Promise<UserEntity[]> {
+  async getAllUser(): Promise<UserEntity[]> {
     return await this.userService.getAllUser();
   }
 
