fb33d0afc98428583009e9b7877b36dd85708174
tsannie <tsannie@student.42.fr>
2022-10-12 12:02:05 +0200

    [fix] snackbar after reload

--- srcs/front_react/src/components/settings/EditUsername.tsx
+++ srcs/front_react/src/components/settings/EditUsername.tsx
@@ -1,64 +1,67 @@
-import React, { Fragment, useContext, useState } from 'react';
-import { AuthContext, AuthContextType } from '../../contexts/AuthContext';
+import React, { Fragment, useContext, useState } from "react";
+import { AuthContext, AuthContextType } from "../../contexts/AuthContext";
 import EditIcon from "../../assets/img/icon/edit.png";
 import VerifIcon from "../../assets/img/icon/verifier.png";
-import { api } from '../../const/const';
-import { SnackbarContext, SnackbarContextType } from '../../contexts/SnackbarContext';
+import { api } from "../../const/const";
+import {
+  SnackbarContext,
+  SnackbarContextType,
+} from "../../contexts/SnackbarContext";
 
 function EditUsername() {
   const { user } = React.useContext(AuthContext) as AuthContextType;
   const [editUsername, setEditUsername] = useState(false);
   const [newUsername, setNewUsername] = useState("");
-  const { setMessage, setOpenSnackbar, setSeverity } = useContext(SnackbarContext) as SnackbarContextType;
-
+  const { setMessage, setOpenSnackbar, setSeverity, setReloadAfter } =
+    useContext(SnackbarContext) as SnackbarContextType;
 
   const handleUsername = () => {
     setEditUsername(!editUsername);
-  }
+  };
 
   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const size_max = 20;
 
     setNewUsername(e.target.value.slice(0, size_max));
-  }
+  };
 
   const handleVerifyUsername = () => {
-
-    api.post('user/edit-username', {username: newUsername}).then(({ data }) => {
-      console.log('success', data);
-      setSeverity("success");
-      setMessage('Username updated');
-      setOpenSnackbar(true);
-    })
-    .catch((error) => {
-      setSeverity("error");
-      setMessage('\'' + newUsername + '\' is already use or invalid');
-      setOpenSnackbar(true);
-      setNewUsername('');
-    });
-  }
+    api
+      .post("user/edit-username", { username: newUsername })
+      .then(({ data }) => {
+        setSeverity("success");
+        setMessage("Username updated");
+        setReloadAfter(true);
+      })
+      .catch((error) => {
+        setSeverity("error");
+        setMessage("'" + newUsername + "' is already use or invalid");
+        setOpenSnackbar(true);
+        setNewUsername("");
+      });
+  };
 
   return (
     <div className="settings__editable">
-    { editUsername === true &&
-      <Fragment>
-        <input
-          id="username"
-          maxLength={20}
-          type="text"
-          value={newUsername}
-          onChange={handleUsernameChange}
-        ></input>
-        <img src={VerifIcon} onClick={handleVerifyUsername}></img>
-      </Fragment>
-    }
-    { editUsername === false &&
-      <Fragment>
-        <span>{user?.username}</span>
-        <img src={EditIcon} onClick={handleUsername}></img>
-      </Fragment>
-    }
-  </div>
+      {editUsername === true && (
+        <Fragment>
+          <input
+            id="username"
+            maxLength={20}
+            type="text"
+            value={newUsername}
+            onChange={handleUsernameChange}
+          ></input>
+          <img src={VerifIcon} onClick={handleVerifyUsername}></img>
+        </Fragment>
+      )}
+      {editUsername === false && (
+        <Fragment>
+          <span>{user?.username}</span>
+          <img src={EditIcon} onClick={handleUsername}></img>
+        </Fragment>
+      )}
+    </div>
   );
 }
 
--- srcs/front_react/src/contexts/SnackbarContext.tsx
+++ srcs/front_react/src/contexts/SnackbarContext.tsx
@@ -1,14 +1,16 @@
 import { AlertColor } from "@mui/material";
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import CustomSnackbar from "../components/snackbar/CustomSnackbar";
 
 export type SnackbarContextType = {
   openSnackbar: boolean;
   message: string;
   severity: AlertColor | undefined;
+  reloadAfter: boolean;
   setOpenSnackbar: (openSuccess: boolean) => void;
   setMessage: (message: string) => void;
   setSeverity: (severity: AlertColor | undefined) => void;
+  setReloadAfter: (reloadAfter: boolean) => void;
 };
 
 export const SnackbarContext = createContext<Partial<SnackbarContextType>>({});
@@ -19,12 +21,31 @@ interface IProps {
 
 export const SnackbarProvider = ({ children }: IProps) => {
   const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [reloadAfter, setReloadAfter] = useState(false);
   const [severity, setSeverity] = useState("" as AlertColor | undefined);
   const [message, setMessage] = useState("");
 
+
+  useEffect(() => {
+    if (reloadAfter === true) {
+      localStorage.setItem('severityKey', JSON.stringify(severity));
+      localStorage.setItem('messageKey', JSON.stringify(message));
+      window.location.reload();
+    }
+  }, [reloadAfter]);
+
+  useEffect(() => {
+    if (localStorage.messageKey && localStorage.severityKey) {
+      setSeverity(JSON.parse(localStorage.getItem('severityKey') || ''));
+      setMessage(JSON.parse(localStorage.getItem('messageKey') || ''));
+      setOpenSnackbar(true);
+      localStorage.clear();
+    }
+  }, []);
+
   return (
     <SnackbarContext.Provider
-      value={{ setOpenSnackbar, setMessage, setSeverity }}
+      value={{ setOpenSnackbar, setMessage, setSeverity, setReloadAfter }}
     >
       {children}
       <CustomSnackbar
@@ -32,6 +53,7 @@ export const SnackbarProvider = ({ children }: IProps) => {
         openSnackbar={openSnackbar}
         message={message}
         severity={severity}
+        reloadAfter={reloadAfter}
       />
     </SnackbarContext.Provider>
   );
