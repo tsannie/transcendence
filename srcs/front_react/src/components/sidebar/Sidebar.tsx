c0946d10c2ac922e7d92ef4fb4d50553aa1abd41
tsannie <tsannie@student.42.fr>
2022-10-20 12:33:53 +0200

    [+] start profilepage

--- srcs/front_react/src/components/sidebar/Sidebar.tsx
+++ srcs/front_react/src/components/sidebar/Sidebar.tsx
@@ -1,52 +1,54 @@
 import { useContext, useEffect, useState } from "react";
-import {ReactComponent as HomeIcon} from "../../assets/img/icon/home.svg";
-import {ReactComponent as ChatIcon} from "../../assets/img/icon/chat.svg";
-import {ReactComponent as GameIcon} from "../../assets/img/icon/play.svg";
-import {ReactComponent as SettingsIcon} from "../../assets/img/icon/settings.svg";
-import {ReactComponent as LogOutIcon} from "../../assets/img/icon/logout.svg";
-import {ReactComponent as ProfileIcon} from "../../assets/img/icon/user.svg";
+import { ReactComponent as HomeIcon } from "../../assets/img/icon/home.svg";
+import { ReactComponent as ChatIcon } from "../../assets/img/icon/chat.svg";
+import { ReactComponent as GameIcon } from "../../assets/img/icon/play.svg";
+import { ReactComponent as SettingsIcon } from "../../assets/img/icon/settings.svg";
+import { ReactComponent as LogOutIcon } from "../../assets/img/icon/logout.svg";
+import { ReactComponent as ProfileIcon } from "../../assets/img/icon/user.svg";
 import { api, COOKIE_NAME } from "../../const/const";
 import "./sidebar.style.scss";
 import { Link } from "react-router-dom";
 import { AuthContext, AuthContextType } from "../../contexts/AuthContext";
-import { useLocation } from "react-router-dom"
-import { SnackbarContext, SnackbarContextType } from "../../contexts/SnackbarContext";
+import { useLocation } from "react-router-dom";
+import {
+  SnackbarContext,
+  SnackbarContextType,
+} from "../../contexts/SnackbarContext";
 
 export default function Sidebar() {
   const { logout } = useContext(AuthContext) as AuthContextType;
   const { setMessage, setOpenSnackbar, setSeverity, setAfterReload } =
-  useContext(SnackbarContext) as SnackbarContextType;
+    useContext(SnackbarContext) as SnackbarContextType;
   const path = useLocation().pathname;
 
   const handleLogout = () => {
-    api.get('/auth/logout')
-      .then(res => {
-        setMessage('bye bye');
-        setSeverity('info');
-        setOpenSnackbar(true);
-        logout();
-      });
-  }
+    api.get("/auth/logout").then((res) => {
+      setMessage("bye bye");
+      setSeverity("info");
+      setOpenSnackbar(true);
+      logout();
+    });
+  };
 
   return (
     <div className="sidebar">
       <nav className="sidebar__icon">
         <Link to="/">
-          <HomeIcon className={path === '/' ? 'selected' : ''}/>
+          <HomeIcon className={path === "/" ? "selected" : ""} />
         </Link>
         <Link to="/profile">
-          <ProfileIcon className={path === '/profile' ? 'selected' : ''}/>
+          <ProfileIcon className={path === "/profile" ? "selected" : ""} />
         </Link>
         <Link to="/chat">
-          <ChatIcon className={path === '/chat' ? 'selected' : ''}/>
+          <ChatIcon className={path === "/chat" ? "selected" : ""} />
         </Link>
         <Link to="/game">
-          <GameIcon className={path === '/game' ? 'selected' : ''}/>
+          <GameIcon className={path === "/game" ? "selected" : ""} />
         </Link>
         <Link to="/settings">
-          <SettingsIcon className={path === '/settings' ? 'selected' : ''}/>
+          <SettingsIcon className={path === "/settings" ? "selected" : ""} />
         </Link>
-        <LogOutIcon onClick={handleLogout} className=''/>
+        <LogOutIcon onClick={handleLogout} className="" />
       </nav>
     </div>
   );
