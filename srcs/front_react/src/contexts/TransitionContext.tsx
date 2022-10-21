d03c4bd3640880e6642cbea21df3c02db2ad1ec0
tsannie <tsannie@student.42.fr>
2022-10-21 10:14:46 +0200

    [+] up and down animation transi

--- srcs/front_react/src/contexts/TransitionContext.tsx
+++ srcs/front_react/src/contexts/TransitionContext.tsx
@@ -2,9 +2,11 @@ import React, { createContext, Dispatch, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 export type TransitionContextType = {
+  enableTransition: boolean;
   transitionStage: string;
   displayLocation: any;
   location: any;
+  setEnableTransition: (enableTransition: boolean) => void;
   setTransistionStage: (stage: string) => void;
   setDisplayLocation: (location: any) => void;
 };
@@ -17,19 +19,60 @@ interface IProps {
   children: JSX.Element | JSX.Element[];
 }
 
+enum TransitionPageLvl {
+  NULL = 0,
+  HOME = 1,
+  PROFILE = 2,
+  CHAT = 3,
+  GAME = 4,
+  SETTINGS = 5,
+}
+
 export const TransitionProvider = ({ children }: IProps) => {
   const location = useLocation();
 
+  const [enableTransition, setEnableTransition] = useState(false);
   const [displayLocation, setDisplayLocation] = useState(location);
-  const [transitionStage, setTransistionStage] = useState("bounce-in-up");
+  const [transitionStage, setTransistionStage] = useState("");
+  const [actualLvl, setActualLvl] = useState(TransitionPageLvl.NULL);
+
+  function getTransitionStage(path: string) {
+    switch (path) {
+      case "/":
+        return TransitionPageLvl.HOME;
+      case "/profile":
+        return TransitionPageLvl.PROFILE;
+      case "/chat":
+        return TransitionPageLvl.CHAT;
+      case "/game":
+        return TransitionPageLvl.GAME;
+      case "/settings":
+        return TransitionPageLvl.SETTINGS;
+      default:
+        return TransitionPageLvl.NULL;
+    }
+  }
 
   useEffect(() => {
-    if (location !== displayLocation) setTransistionStage("bounce-in-down");
+    console.log("location changed:", location);
+
+    const newLvl = getTransitionStage(location.pathname);
+
+    if (actualLvl < newLvl) {
+      console.log("go down");
+      setTransistionStage("exit-up");
+    } else if (actualLvl > newLvl) {
+      console.log("go up");
+      setTransistionStage("exit-down");
+    }
+    setActualLvl(newLvl);
   }, [location, displayLocation]);
 
   return (
     <TransitionContext.Provider
       value={{
+        setEnableTransition,
+        enableTransition,
         displayLocation,
         transitionStage,
         setTransistionStage,
