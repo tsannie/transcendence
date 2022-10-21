1508a2d0c6f53b586bd56f0d2742f9ff5a9429c8
philippebarradas <philippe.lb.barradas@gmail.com>
2022-10-21 13:27:23 +0200

    [fix] paddle responsive

--- srcs/back_nestjs/src/game/const/const.ts
+++ srcs/back_nestjs/src/game/const/const.ts
@@ -1,11 +1,11 @@
 
 
-export const canvas_back_width: number = 1080;
+export const canvas_back_width: number = 1920;
 
 export const screen_ratio: number = 16/9;
 
 export const canvas_back_height: number = canvas_back_width * screen_ratio;
 
 export const paddle_margin_position: number = 50;
-export const paddle_width: number = 50;
-export const paddle_height: number = 400;
+export const paddle_width: number = 100;
+export const paddle_height: number = 800;
