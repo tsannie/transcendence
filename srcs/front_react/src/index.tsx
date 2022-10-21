5ef42ad11a7296b034d8a5e7be46fa2ce92484b3
dodjian <dovdjianpro@gmail.com>
2022-10-21 13:28:14 +0200

    [+] start to remove user context

--- srcs/front_react/src/index.tsx
+++ srcs/front_react/src/index.tsx
@@ -5,13 +5,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
 import { SnackbarProvider } from "./contexts/SnackbarContext";
+import { SocketProvider } from "./contexts/SocketContext";
 
 const root = createRoot(document.getElementById("root")!);
 
 root.render(
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <SocketProvider>
+        <App />
+      </SocketProvider>
     </AuthProvider>
   </BrowserRouter>
 );
