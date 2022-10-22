6f268d848483eddd9a47d4aeaaab450956e1b395
tsannie <tsannie@student.42.fr>
2022-10-22 08:49:34 +0200

    [-] clean

--- srcs/front_react/src/index.tsx
+++ srcs/front_react/src/index.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
-import { SnackbarProvider } from "./contexts/SnackbarContext";
 import { TransitionProvider } from "./contexts/TransitionContext";
 
 const root = createRoot(document.getElementById("root")!);
