e9aa29d9506dbbe2ac6e4d3a705bad3c6f55412d
tsannie <tsannie@student.42.fr>
2022-10-23 21:49:21 +0200

    [+] implement oauthgoogle

--- srcs/back_nestjs/src/auth/service/auth.service.ts
+++ srcs/back_nestjs/src/auth/service/auth.service.ts
@@ -30,6 +30,22 @@ export class AuthService {
     return await this.userService.add42DefaultAvatar(profile42._json.image_url, new_user);
   }
 
+  async validateUserGoogle(profile: any): Promise<any> {
+    const user = await this.userService.findByMail(profile.emails[0].value);
+    if (user)
+      return user;
+    if (profile.email_verified !== true) {
+      throw new UnauthorizedException('Account not verified');
+    }
+
+    let new_user = new UserEntity();
+    new_user.username = profile.displayName;
+    new_user.email = profile.emails[0].value;
+    new_user = await this.register(new_user);
+
+    return await this.userService.add42DefaultAvatar(profile._json.image_url, new_user);
+  }
+
   async register(user: UserEntity): Promise<UserEntity> {
     return await this.userService.add(user);
   }
