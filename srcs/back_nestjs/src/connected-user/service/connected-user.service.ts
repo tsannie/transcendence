2efbd4de7ccd6e0a46ab49a0577415dbf524af28
dodjian <dovdjianpro@gmail.com>
2022-10-25 14:13:29 +0200

    [-] cleaning code

--- srcs/back_nestjs/src/connected-user/service/connected-user.service.ts
+++ srcs/back_nestjs/src/connected-user/service/connected-user.service.ts
@@ -7,29 +7,16 @@ import { ConnectedUserDto } from '../dto/connected-user.dto';
 
 @Injectable()
 export class ConnectedUserService {
-
   constructor(
     @InjectRepository(ConnectedUserEntity)
-    private readonly connectedUserRepository: Repository<ConnectedUserEntity>
-  ) { }
+    private readonly connectedUserRepository: Repository<ConnectedUserEntity>,
+  ) {}
 
   async create(connectedUser: ConnectedUserDto): Promise<ConnectedUserDto> {
     return this.connectedUserRepository.save(connectedUser);
   }
 
-  /* async findByUser(user: ConnectedUserEntity): Promise<ConnectedUserDto[]> {
-    return this.connectedUserRepository.find({ user });
-  } */
-
   async deleteBySocketId(socketId: string) {
     return this.connectedUserRepository.delete({ socketId });
   }
-
-  async deleteAll() {
-    await this.connectedUserRepository
-      .createQueryBuilder()
-      .delete()
-      .execute();
-  }
-
 }
