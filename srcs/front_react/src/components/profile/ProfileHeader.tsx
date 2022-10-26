88db00d5b440ce39d386b618310efa8b37009a24
tsannie <tsannie@student.42.fr>
2022-10-26 17:56:12 +0200

    [+] start searchbar

--- srcs/front_react/src/components/profile/ProfileHeader.tsx
+++ srcs/front_react/src/components/profile/ProfileHeader.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { ReactComponent as MedalIcon } from "../../assets/img/icon/medal.svg";
-import { ReactComponent as SearchIcon } from "../../assets/img/icon/search.svg";
 import { User } from "../../contexts/AuthContext";
+import SearchBar from "./SearchBar";
 
 interface IProps {
   user: User | null;
 }
 
 function ProfileHeader(props: IProps) {
-
   return (
     <div className="profile__header">
       <div className="profile__header__user">
         <div className="profile__header__user__avatar">
-          <img src={props.user?.profile_picture + "&size=medium"} alt="avatar" />
+          <img
+            src={props.user?.profile_picture + "&size=medium"}
+            alt="avatar"
+          />
         </div>
         <div className="profile__header__user__info">
           <h2>{props.user?.username}</h2>
@@ -25,10 +27,7 @@ function ProfileHeader(props: IProps) {
       </div>
       <div className="profile__header__search">
         <span>search player:</span>
-        <div className="profile__header__search__item">
-          <SearchIcon />
-          <input type="text" placeholder="pseudo" />
-        </div>
+        <SearchBar />
       </div>
     </div>
   );
