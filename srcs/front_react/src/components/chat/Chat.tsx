2e355ca9b38ab532f504bda1ac14bb9bbb75f4e0
Guillaume PETIT <gpetit@e1r2p6.clusters.42paris.fr>
2022-10-28 18:25:38 +0200

    [+] added dm and channel list and a bit of style

--- srcs/front_react/src/components/chat/Chat.tsx
+++ srcs/front_react/src/components/chat/Chat.tsx
@@ -1,19 +1,48 @@
 import { Box, Grid, Popover, Typography } from "@mui/material";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { IChannel, IMessageReceived } from "./types";
 import { api, COOKIE_NAME } from "../../const/const";
-import { ChatProvider } from "../../contexts/ChatContext";
+import { ChatList, ChatListProvider } from "../../contexts/ChatContext";
 import "./chat.style.scss"
+import { AuthContext, AuthContextType } from "../../contexts/AuthContext";
+
+
+function MessageList() {
+  const { user } = useContext(AuthContext) as AuthContextType;
+  const [chatList, setChatList] = useState<IChannel[]>([]);
+
+  const loadList = async () => {
+    await api
+    .get("/user/conversations")
+    .then((res) => {
+      console.log("DATAAAAAS = ", res.data)
+      setChatList(res.data);
+    })
+    .catch( () => console.log("Error"));
+  }
+
+  const MessageListItems = chatList.map( (conv: any) => {
+      if (conv.name)
+          return <li className="chat__list__items" key={conv.id}>{conv.name}</li>
+      else
+          return (<li className="chat__list__items" key={conv.id}>{conv.users[0].username !== user?.username ? conv.users[0].username : conv.users[1].username}</li>)
+    });
+
+    useEffect( () => {
+      loadList();
+    }, [])
+
+  return (<ul className="chat__list__body">{MessageListItems}</ul>)
+}
 
 function Chat() {
-
-
   return (
-    <ChatProvider>
       <div className="chat">
-      <div className="chat__list"></div>
+        <div className="chat__list">
+          <div className="chat__list__header">Channels</div>
+          <MessageList />
+        </div>
       </div>
-    </ChatProvider>
   );
 }
 
