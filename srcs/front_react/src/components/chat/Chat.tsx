6a370162ce8a827c37c863e52cbab6d2d734c731
Guillaume PETIT <gpetit@e1r1p3.clusters.42paris.fr>
2022-10-31 17:07:45 +0100

    [save] socket on working and front is ok

--- srcs/front_react/src/components/chat/Chat.tsx
+++ srcs/front_react/src/components/chat/Chat.tsx
@@ -3,7 +3,6 @@ import { IChannel } from "./types";
 import { api } from "../../const/const";
 import "./chat.style.scss"
 import { AuthContext, AuthContextType, AuthProvider, User } from "../../contexts/AuthContext";
-import { io } from "socket.io-client";
 import { SocketContext, SocketProvider } from "../../contexts/SocketContext";
 
 interface IProps {
@@ -20,25 +19,55 @@ function MessageList() {
     await api
     .get("/user/conversations")
     .then((res) => {
-      console.log("DATAAAAAS = ", res.data)
+      res.data.forEach((channel: IChannel) => {
+        channel.notif = false;
+      });
       setChatList(res.data);
     })
-    .catch( () => console.log("Error"));
+    .catch( () => console.log("Axios Error"));
+  }
+
+  const updateList = () => {
+    let newList = [...chatList];
+    let conv_id = message?.channel ? message?.channel.id : message?.dm.id;
+    let editable_room = newList.find( (elem) => elem.id === conv_id);
+    if (editable_room && message)
+    {
+      editable_room.notif = true;
+      editable_room.updatedAt = message.createdAt;
+    }
+    newList.sort( (a,b) => {
+      if (a.updatedAt < b.updatedAt)
+        return 1;
+      else
+        return -1;
+    })
+    setChatList(newList);
   }
 
   const MessageListItems = chatList.map( (conv: any) => {
-      if (conv.name)
-          return <li className="chat__list__items" key={conv.id}>{conv.name}</li>
+    /* Imbricated ternary operators. First one is to distinguate DM from Channel, and second one is to get the name of other user, than us */
+      const conv_title = conv.name ? conv.name : (conv.users[0].username !== user?.username ? conv.users[0].username : conv.users[1].username);
+      
+      if (conv.notif)
+          return (
+          <li className="chat__list__items" key={conv.id}>
+            <div className="notif" />
+              {conv_title}
+          </li>
+          )
       else
-          return (<li className="chat__list__items" key={conv.id}>{conv.users[0].username !== user?.username ? conv.users[0].username : conv.users[1].username}</li>)
+        return <li className="chat__list__items" key={conv.id}>{conv_title}</li>
     });
 
     //EQUIVALENT TO COMPONENTDIDMOUNT
     useEffect( () => {
-      console.log("USER = ", user);
-      console.log("message = ", message);
       loadList();
     }, []);
+
+    useEffect( () => {
+      updateList();
+    }, [message])
 
   return (<ul className="chat__list__body">{MessageListItems}</ul>)
 }
