8e2458e31be51c4122946769996a2c789b5f3381
tsannie <tsannie@student.42.fr>
2022-11-03 18:22:01 +0100

    [+] start front request friends

--- srcs/front_react/src/components/profile/ProfileFriends.tsx
+++ srcs/front_react/src/components/profile/ProfileFriends.tsx
@@ -1,6 +1,8 @@
 import userEvent from "@testing-library/user-event";
-import React from "react";
+import React, { Fragment } from "react";
 import { User } from "../../contexts/AuthContext";
+import { ReactComponent as AddIcon } from "../../assets/img/icon/circle_check.svg";
+import { ReactComponent as RemoveIcon } from "../../assets/img/icon/circle_remove.svg";
 
 interface IProps {
   user: User | null;
@@ -8,6 +10,7 @@ interface IProps {
 }
 
 function ProfileFriends(props: IProps) {
+  let allFriendRequests;
   let allFriends = props.user?.friends.map((friend, index) => {
     return (
       <img
@@ -18,7 +21,26 @@ function ProfileFriends(props: IProps) {
     );
   });
 
-  console.log(props.user?.friend_requests);
+  if (props.isPerso) {
+    allFriendRequests = props.user?.friend_requests.map((request, index) => {
+      return (
+        <Fragment key={index}>
+          <div className="friend__request__item">
+            <div className="info__request">
+              <img src={request.profile_picture + "&size=small"} alt="avatar" />
+              <span>
+                {request.username.substring(0, 10)}
+                {request.username.length > 10 ? "..." : ""}
+              </span>
+            </div>
+            <AddIcon />
+            <RemoveIcon />
+          </div>
+          <hr />
+        </Fragment>
+      );
+    });
+  }
 
   return (
     <div className="profile__body__friends">
@@ -28,7 +50,9 @@ function ProfileFriends(props: IProps) {
       </div>
       <hr id="full" />
       {props.isPerso && props.user?.friend_requests ? (
-        <>test</>
+        <div className="profile__body__friend__request">
+          {allFriendRequests}
+        </div>
       ) : (
         <div className="profile__body__friends__list">{allFriends}</div>
       )}
