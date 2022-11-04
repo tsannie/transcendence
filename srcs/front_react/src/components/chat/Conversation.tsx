e4fb333dd1fd4247b3e3845192407ccc5693d4b9
Guillaume PETIT <gpetit@e1r11p7.clusters.42paris.fr>
2022-11-04 13:13:52 +0100

    [prettier] divided code logic, and DM and CHANNEL are now using the same Conversation component

---
+++ srcs/front_react/src/components/chat/Conversation.tsx
@@ -0,0 +1,12 @@
+import Channel from "./Channel";
+import Options from "./Options";
+
+function Conversation() {
+    return (
+      <div className="conversation">
+        <Channel />
+        <Options />
+      </ div>);
+  }
+
+  export default Conversation;
