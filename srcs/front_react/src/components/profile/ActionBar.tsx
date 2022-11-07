b4e54710af1524aed7d85417298f5d31d07f14ff
tsannie <tsannie@student.42.fr>
2022-11-07 14:06:53 +0100

    start add friend

--- srcs/front_react/src/components/profile/ActionBar.tsx
+++ srcs/front_react/src/components/profile/ActionBar.tsx
@@ -2,8 +2,23 @@ import React from "react";
 import { ReactComponent as ChatIcon } from "../../assets/img/icon/chat.svg";
 import { ReactComponent as UserIcon } from "../../assets/img/icon/user.svg";
 import { ReactComponent as BlockIcon } from "../../assets/img/icon/no_waiting_sign.svg";
+import { User } from "../../contexts/AuthContext";
+import { api } from "../../const/const";
 
-function ActionBar() {
+interface IProps {
+  player: User | null;
+  user: User | null;
+}
+
+function ActionBar(props: IProps) {
+  const handleAddFriend = () => {
+    api
+      .post("/user/add_friend", { username: props.player?.username })
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
   return (
     <div className="action-bar">
       <div className="action-bar__item">
