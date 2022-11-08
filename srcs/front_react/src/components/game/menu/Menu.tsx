8056f41641e3679fd724596e33cb02ce7f6c97bd
philippebarradas <philippe.lb.barradas@gmail.com>
2022-11-08 17:16:12 +0100

    [+] giveup while countdown

--- srcs/front_react/src/components/game/menu/Menu.tsx
+++ srcs/front_react/src/components/game/menu/Menu.tsx
@@ -1,16 +1,33 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { socket } from "../Game";
 import { GameContext, RoomStatus } from "../GameContext";
 import { GamePlayer_p1_p2 } from "../render/Render";
 import WaitingRoom from "./WaitingRoom";
 
+enum GameMode {
+  PONG_1972 = "1972",
+  PONG_TRANS = "TRANS",
+}
+
 export default function GameMenu(props: any) {
 
   const game = useContext(GameContext);
+  let game_mode = GameMode.PONG_1972;
+  
+  function createGameRoom() {
+    let data = {
+      room : game.room,
+      game_mode : game_mode,
+    }
 
-  function createFastGameRoom() {
+    game.setRoom("");
     if (game.status === RoomStatus.EMPTY)
-      socket.emit("createGameRoom", game.room);
+      socket.emit("createGameRoom", data);
+  }
+
+  function createGameRoomTRANS() {
+    game_mode = GameMode.PONG_TRANS;
+    createGameRoom();
   }
 
   return (
@@ -19,10 +36,10 @@ export default function GameMenu(props: any) {
       <div className="GameMenu">
         <h2> you are : {game.my_id} </h2>
         <br />
-        <button onClick={createFastGameRoom}>PONG 1972</button>
+        <button onClick={createGameRoom}>PONG 1972</button>
         <br/>
         <br/>
-        <button onClick={createFastGameRoom}>PONG TRANSCENDENCE</button>
+        <button onClick={createGameRoomTRANS}>PONG TRANSCENDENCE</button>
       </div>
     }
 
