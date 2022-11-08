83e0ddd1d81cc4e20bb63d0635144546cd2b0be0
Guillaume PETIT <gpetit@e1r4p7.clusters.42paris.fr>
2022-11-08 13:35:02 +0100

    [save] pause midi

--- srcs/front_react/src/components/chat/CreateChannelForm.tsx
+++ srcs/front_react/src/components/chat/CreateChannelForm.tsx
@@ -1,14 +1,19 @@
 function CreateChannelForm () {
+    const createChannel = () => {
+        return ;
+    }
+
     return (<div className="create__channel">
-            <h2>CREATE CHANNEL</h2>
-            <form>
-                <input type="text" placeholder="Enter Name of New Channel..." />
-                <div className="create__chan__status">
-                    <button>public</button>
-                    <button>private</button>
-                    <button>protected</button>
-                </div>
-            </form>
+                <h2>CREATE CHANNEL</h2>
+                <form onSubmit={createChannel}>
+                    <input type="text" placeholder="Enter Name of New Channel..." />
+                    <div className="create__chan__status">
+                        <button>public</button>
+                        <button>private</button>
+                        <button>protected</button>
+                    </div>
+                </form>
+                <button className="create__chan__validator">create channel</button>
         </div>)
 }
 
