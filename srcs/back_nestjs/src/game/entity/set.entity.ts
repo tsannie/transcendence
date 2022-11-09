fed59aa63420f5c1efe21d88910c1e936775b903
philippebarradas <philippe.lb.barradas@gmail.com>
2022-11-09 20:01:22 +0100

    [+] MAXI CLEAN CODE

--- srcs/back_nestjs/src/game/entity/set.entity.ts
+++ srcs/back_nestjs/src/game/entity/set.entity.ts
@@ -5,17 +5,12 @@ import {
   OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
-import { BallEntity } from './ball.entity';
 import { PlayerEntity } from './players.entity';
 
 @Entity()
 export class SetEntity {
   @PrimaryGeneratedColumn()
   id?: number;
-
-  @OneToOne(() => BallEntity, { eager: true, cascade: true })
-  @JoinColumn()
-  ball: BallEntity;
 
   @OneToOne(() => PlayerEntity, { eager: true, cascade: true })
   @JoinColumn()
