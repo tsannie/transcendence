4c75868a9a7b4bca51c6b4ac79a73af48ecf6731
Guillaume PETIT <gpetit@e1r13p1.clusters.42paris.fr>
2022-11-09 14:40:19 +0100

    [save]

--- srcs/front_react/src/components/chat/Options.tsx
+++ srcs/front_react/src/components/chat/Options.tsx
@@ -1,12 +1,45 @@
-import { useContext } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../../const/const";
 import { ChatDisplayContext } from "../../contexts/ChatDisplayContext";
+import { IDm } from "./types";
+
+function DmOptions() {
+  const { currentConv } = useContext(ChatDisplayContext);
+  const [dm, setDm] = useState<IDm>({} as IDm);
+
+  const loadDm = async () => {
+    await api
+      .get("dm/datas")
+      .then((res) => {
+        console.log(res.data);
+        setDm(res.data);
+      })
+      .catch((err) => {toast.error("HTTP error:" + err)});
+  };
+
+  useEffect(() => {
+    const async_fct = async () => { await loadDm() };
+
+    async_fct();
+  }, []);
+
+  return <Fragment>
+      <div className="conversation__options__title">
+      </div>
+    </Fragment>
+}
+
+function ChannelOptions() {
+  return <Fragment></Fragment>
+}
 
 function Options() {
-  const { currentConv } = useContext(ChatDisplayContext);
+  const { isChannel } = useContext(ChatDisplayContext);
 
   return (
     <div className="conversation__options">
-      <div className="conversation__options__title"></div>
+      {isChannel? <ChannelOptions /> : <DmOptions />}
     </div>
   );
 }
