ac1965b3d8b0e4dfde9e6345a34d79cf4283b307
tsannie <tsannie@student.42.fr>
2022-11-09 12:54:57 +0100

    [fix] context

--- srcs/front_react/src/components/chat/Channel.tsx
+++ srcs/front_react/src/components/chat/Channel.tsx
@@ -1,12 +1,11 @@
-import "./chat.style.scss"
+import "./chat.style.scss";
 import { MessageContext } from "../../contexts/MessageContext";
 import { ChatDisplayContext } from "../../contexts/ChatDisplayContext";
 import { Fragment, useContext, useEffect, useRef, useState } from "react";
 import { IChannel, IDm, IMessageReceived, IMessageSent } from "./types";
 import { api } from "../../const/const";
 import { AuthContext, AuthContextType, User } from "../../contexts/AuthContext";
-import  {ReactComponent as SendIcon} from "../../assets/img/icon/send.svg";
-
+import { ReactComponent as SendIcon } from "../../assets/img/icon/send.svg";
 
 function MessageForm(props: any) {
   const convId = props.convId;
@@ -17,129 +16,134 @@ function MessageForm(props: any) {
 
   const actualize_input = (event: any) => {
     setInput(event.target.value);
-  }
+  };
 
   const sendMessage = async (event: any) => {
     event.preventDefault();
-    if (input === "")
-      return ;
-    const data : IMessageSent = {
-        convId: convId,
-        content: input,
-        isDm: !isChannel,
-    }
+    if (input === "") return;
+    const data: IMessageSent = {
+      convId: convId,
+      content: input,
+      isDm: !isChannel,
+    };
     socket?.emit("message", data);
     setInput("");
-  }
+  };
 
   return (
     <form onSubmit={sendMessage}>
-        <input className="input__form" type="text" placeholder="add message..." value={input} onChange={actualize_input} />
-          <SendIcon className="send__button" onClick={sendMessage}/>
-    </ form>
-  )
+      <input
+        className="input__form"
+        type="text"
+        placeholder="add message..."
+        value={input}
+        onChange={actualize_input}
+      />
+      <SendIcon className="send__button" onClick={sendMessage} />
+    </form>
+  );
 }
 
 function MessageList(props: any) {
-    const user : User = props.user
-    const messages : IMessageReceived[] = props.messages;
+  const user: User = props.user;
+  const messages: IMessageReceived[] = props.messages;
 
-    return <Fragment>
-        {messages.map( (message: IMessageReceived) => { 
-          let items_class : string;
-    
-          if (message.author?.id === user?.id)
-            items_class = "conversation__messages__items self";
-          else
-            items_class = "conversation__messages__items other";
-          return <li className={items_class} key={message.id}>
-                    <img src={message.author?.profile_picture + "&size=small"} className="avatar" />
-                    <div className="conversation__messages__text">{message.content}</div>
-                  </ li>
-        })}
-      </Fragment>;
+  return (
+    <Fragment>
+      {messages.map((message: IMessageReceived) => {
+        let items_class: string;
+
+        if (message.author?.id === user?.id)
+          items_class = "conversation__messages__items self";
+        else items_class = "conversation__messages__items other";
+        return (
+          <li className={items_class} key={message.id}>
+            <img
+              src={message.author?.profile_picture + "&size=small"}
+              className="avatar"
+            />
+            <div className="conversation__messages__text">
+              {message.content}
+            </div>
+          </li>
+        );
+      })}
+    </Fragment>
+  );
 }
 
 function MessageBody() {
-    const { user } = useContext(AuthContext) as AuthContextType;
-    const { currentConvId, isChannel } = useContext(ChatDisplayContext);
-    const { newMessage } = useContext(MessageContext);
-    const messagesEndRef = useRef<null | HTMLDivElement>(null);
-    
-    const [offset, setOffset] = useState<number>(0);
-    const [messages, setMessages] = useState<IMessageReceived[]>([]);
+  const { user } = useContext(AuthContext) as AuthContextType;
+  const { currentConv, isChannel } = useContext(ChatDisplayContext);
+  const { newMessage } = useContext(MessageContext);
+  const messagesEndRef = useRef<null | HTMLDivElement>(null);
 
-  
+  const [offset, setOffset] = useState<number>(0);
+  const [messages, setMessages] = useState<IMessageReceived[]>([]);
+
   const scrollToBottom = (smooth: boolean = false) => {
-      setTimeout(() => {
-        if (!smooth)
-          messagesEndRef.current?.scrollIntoView(false);
-        else
-          messagesEndRef.current?.scrollIntoView({behavior: "smooth"})
-      }
-        , 200);
-    }
+    setTimeout(() => {
+      if (!smooth) messagesEndRef.current?.scrollIntoView(false);
+      else messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 200);
+  };
 
   const loadMessage = async () => {
-    let route : string;
+    let route: string;
 
-    if (isChannel)
-        route = "/message/channel";
-    else
-        route = "/message/dm";
-    
+    if (isChannel) route = "/message/channel";
+    else route = "/message/dm";
+
     await api
-      .get(route, {params: {id: currentConvId, offset: offset}})
+      .get(route, { params: { id: currentConv, offset: offset } })
       .then((res) => {
-        const unorderedMessages : IMessageReceived[] = res.data;
-        setMessages(unorderedMessages.sort( (a, b) => {
-          if (a.createdAt < b.createdAt)
-            return -1;
-          else
-            return 1;
-        }
-        ));
+        const unorderedMessages: IMessageReceived[] = res.data;
+        setMessages(
+          unorderedMessages.sort((a, b) => {
+            if (a.createdAt < b.createdAt) return -1;
+            else return 1;
+          })
+        );
         setOffset(0);
       })
-      .catch( () => console.log("Axios Error"));
-  }
+      .catch(() => console.log("Axios Error"));
+  };
 
   const addMessage = (newMessage: IMessageReceived | null) => {
-    if (newMessage)
-      setMessages([...messages, newMessage]);
-  }
+    if (newMessage) setMessages([...messages, newMessage]);
+  };
 
-
-  useEffect( () => {
+  useEffect(() => {
     const async_func = async () => {
       await loadMessage();
     };
-    
+
     async_func();
     scrollToBottom();
-  }, [currentConvId]);
+  }, [currentConv]);
 
-  useEffect ( () => {
-    if (newMessage && (newMessage?.dm?.id == currentConvId || newMessage?.channel?.id == currentConvId))
-    { 
+  useEffect(() => {
+    if (
+      newMessage &&
+      (newMessage?.dm?.id == currentConv ||
+        newMessage?.channel?.id == currentConv)
+    ) {
       addMessage(newMessage);
       scrollToBottom(true);
     }
   }, [newMessage]);
 
-
-
   return (
-      <Fragment>
-        <div className="conversation__elems">
-          <ul className="conversation__messages__list">
-            <MessageList messages={messages} user={user}/>
-            <div ref={messagesEndRef}/>
-          </ul>
-          <MessageForm convId={currentConvId} isChannel={isChannel} />
-        </div>
-      </ Fragment>
-      );
+    <Fragment>
+      <div className="conversation__elems">
+        <ul className="conversation__messages__list">
+          <MessageList messages={messages} user={user} />
+          <div ref={messagesEndRef} />
+        </ul>
+        <MessageForm convId={currentConv} isChannel={isChannel} />
+      </div>
+    </Fragment>
+  );
 }
 
 export default MessageBody;
--- srcs/front_react/src/components/chat/MessageList.tsx
+++ srcs/front_react/src/components/chat/MessageList.tsx
@@ -1,173 +1,182 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import { IChannel, IDm, IMessageReceived } from "./types";
 import { api } from "../../const/const";
-import "./chat.style.scss"
+import "./chat.style.scss";
 import { AuthContext, AuthContextType, User } from "../../contexts/AuthContext";
 import { MessageContext } from "../../contexts/MessageContext";
-import { ChatDisplayContext, ChatStateProvider, ChatType } from "../../contexts/ChatDisplayContext";
-import  {ReactComponent as GroupChatIcon} from "../../assets/img/icon/user.svg";
+import {
+  ChatDisplayContext,
+  ChatStateProvider,
+  ChatType,
+} from "../../contexts/ChatDisplayContext";
+import { ReactComponent as GroupChatIcon } from "../../assets/img/icon/user.svg";
 import { NotifContext } from "../../contexts/ChatNotificationContext";
-import {ReactComponent as CirclePlusIcon} from "../../assets/img/icon/circle_plus.svg";
+import { ReactComponent as CirclePlusIcon } from "../../assets/img/icon/circle_plus.svg";
 
 function MessageList() {
-    const { user } = useContext(AuthContext) as AuthContextType;
-    const { newMessage } = useContext(MessageContext);
-    const { channels, addChannel, changeNotif, isNotif } = useContext(NotifContext);
-    const { currentConvId, isChannel, changeDisplay, changeCurrentConv, changeIsChannel, newConv } = useContext(ChatDisplayContext);
-    const messagesTopRef = useRef<null | HTMLDivElement>(null);
+  const { user } = useContext(AuthContext) as AuthContextType;
+  const { newMessage } = useContext(MessageContext);
+  const { channels, addChannel, changeNotif, isNotif } =
+    useContext(NotifContext);
+  const {
+    currentConv,
+    isChannel,
+    setDisplay,
+    setCurrentConv,
+    setIsChannel,
+    newConv,
+  } = useContext(ChatDisplayContext);
+  const messagesTopRef = useRef<null | HTMLDivElement>(null);
 
-    const [chatList, setChatList] = useState<(IChannel | IDm)[]>([]);
-  
-    const scrollToTop = () => {
-      setTimeout( () => {
-        messagesTopRef.current?.scrollIntoView({ behavior: "smooth" });
-      }, 200);
-    }
+  const [chatList, setChatList] = useState<(IChannel | IDm)[]>([]);
 
-    const loadList = async () => {
-      await api
+  const scrollToTop = () => {
+    setTimeout(() => {
+      messagesTopRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 200);
+  };
+
+  const loadList = async () => {
+    await api
       .get("/user/conversations")
       .then((res) => {
         const copy_data: IChannel[] = res.data;
         copy_data.forEach((channel: IChannel) => {
           addChannel(channel.id, false);
-        })
+        });
         setChatList(copy_data);
       })
-      .catch( () => console.log("Axios Error"));
-    }
-    
-    const actualizeChannelList = (newList : (IChannel | IDm)[], editable_room : IChannel | IDm) => {
-      if (newMessage)
-        editable_room.updatedAt = newMessage.createdAt;
-      if (currentConvId && editable_room.id != currentConvId)
-        changeNotif(editable_room.id, true);
-      newList.sort( (a,b) => {
-        if (a.updatedAt < b.updatedAt)
-          return 1;
-        else
-          return -1;
-      })
-      return newList;
-    }
+      .catch(() => console.log("Axios Error"));
+  };
 
-    const addToChannelList = (newList : (IChannel | IDm)[]) => {
-      let new_elem;
+  const actualizeChannelList = (
+    newList: (IChannel | IDm)[],
+    editable_room: IChannel | IDm
+  ) => {
+    if (newMessage) editable_room.updatedAt = newMessage.createdAt;
+    if (currentConv && editable_room.id != currentConv)
+      changeNotif(editable_room.id, true);
+    newList.sort((a, b) => {
+      if (a.updatedAt < b.updatedAt) return 1;
+      else return -1;
+    });
+    return newList;
+  };
 
-      if (newMessage?.dm)
-        new_elem = newMessage.dm;
-      if (newMessage?.channel)
-        new_elem = newMessage?.channel;
-      addChannel(new_elem.id, true);
-      newList = [new_elem, ...newList]
-      return newList;
+  const addToChannelList = (newList: (IChannel | IDm)[]) => {
+    let new_elem;
+
+    if (newMessage?.dm) new_elem = newMessage.dm;
+    if (newMessage?.channel) new_elem = newMessage?.channel;
+    addChannel(new_elem.id, true);
+    newList = [new_elem, ...newList];
+    return newList;
+  };
+
+  const updateList = () => {
+    if (!newMessage) return;
+    else
+      var received_id = newMessage.channel
+        ? newMessage.channel.id
+        : newMessage.dm.id;
+
+    let newList = [...chatList];
+    let editable_room = newList.find((elem) => elem.id === received_id);
+
+    if (editable_room) newList = actualizeChannelList(newList, editable_room);
+    else newList = addToChannelList(newList);
+    setChatList(newList);
+  };
+
+  const clickItem = (conv: IChannel) => {
+    setDisplay(ChatType.CONV);
+    setCurrentConv(conv.id);
+    if (conv.name) setIsChannel(true);
+    else setIsChannel(false);
+    changeNotif(conv.id, false);
+  };
+
+  const displayNotif = (convId: string) => {
+    if (isNotif(convId)) return <div className="notif" />;
+    return null;
+  };
+
+  const MessageListItems = chatList.map((conv: any) => {
+    let title: string | undefined;
+    let user2: User | null = null;
+
+    if (conv.name) title = conv.name;
+    else {
+      user2 =
+        conv.users[0].username !== user?.username
+          ? conv.users[0]
+          : conv.users[1];
+      title = user2?.username;
     }
-  
-    const updateList = () => {
-      if (!newMessage)
-        return;
-      else
-        var received_id = newMessage.channel ? newMessage.channel.id : newMessage.dm.id;
+    return (
+      <div className="chat__list__items" key={conv.id}>
+        <li onClick={() => clickItem(conv)}>
+          <div className="avatar">
+            {user2 ? (
+              <img src={user2.profile_picture + "&size=small"} />
+            ) : (
+              <GroupChatIcon />
+            )}
+          </div>
+          <div className="text__notif">
+            <span>{title}</span>
+            {displayNotif(conv.id)}
+          </div>
+        </li>
+      </div>
+    );
+  });
 
-      let newList = [...chatList];
-      let editable_room = newList.find( (elem) => elem.id === received_id);
-      
-      if (editable_room)
-        newList = actualizeChannelList(newList, editable_room);
-      else
-        newList = addToChannelList(newList);
+  const addNewElemToList = () => {
+    if (chatList.find((elem) => elem.id === currentConv) || !isChannel) return;
+    else {
+      let newList = [newConv, ...chatList];
       setChatList(newList);
     }
+  };
 
-    const clickItem = (conv: IChannel) => {
-      changeDisplay(ChatType.CONV);
-      changeCurrentConv(conv.id);
-      if (conv.name)
-        changeIsChannel(true);
-      else
-        changeIsChannel(false);
-      changeNotif(conv.id, false);
-     }
+  useEffect(() => {
+    const async_fct = async () => await loadList();
 
-    const displayNotif = (convId: string) => {
-      if (isNotif(convId))
-        return <div className="notif" />;
-      return null;
-    }
-  
-    const MessageListItems = chatList.map( (conv: any) => {
-        let title : string | undefined;
-        let user2: User | null = null;
+    async_fct();
+  }, []);
 
+  useEffect(() => {
+    updateList();
+    scrollToTop();
+  }, [newMessage]);
 
-        if (conv.name)
-          title = conv.name;
-        else
-        {
-          user2 = conv.users[0].username !== user?.username ? conv.users[0] : conv.users[1];
-          title = user2?.username;
-        }
-        return (
-          <div className="chat__list__items" key={conv.id}>
-            <li onClick={ () => clickItem(conv)}>
-              <div className="avatar">
-                {user2 ? <img src={user2.profile_picture + "&size=small"} /> : <GroupChatIcon />}
-              </div>
-              <div className="text__notif">
-                <span>{title}</span>
-                {displayNotif(conv.id)}
-              </ div>
-            </li>
-          </ div>
-            )
-      });
+  useEffect(() => {
+    addNewElemToList();
+  }, [newConv]);
 
-      const addNewElemToList = () => {
-        if (chatList.find( (elem) => elem.id === currentConvId) || !isChannel)
-          return;
-        else
-        {
-          let newList = [newConv, ...chatList];
-          setChatList(newList);
-        }
-      }
-  
-      useEffect( () => {
-        const async_fct = async () => await loadList(); 
-        
-        async_fct();
-      }, []);
-  
-      useEffect( () => {
-        updateList();
-        scrollToTop();
-      }, [newMessage]);
-
-      useEffect( () => {
-        addNewElemToList();
-      }, [newConv]);
-
-    return (
+  return (
     <div className="chat__list">
       <div className="chat__list__header">Channels</div>
       <ul className="chat__list__body">
         <div ref={messagesTopRef} />
         {MessageListItems}
       </ul>
-      < CreateChannelButton />
-    </div>)
-  }
+      <CreateChannelButton />
+    </div>
+  );
+}
 
-  function CreateChannelButton() {
-    const { changeDisplay } = useContext(ChatDisplayContext);
+function CreateChannelButton() {
+  const { setDisplay } = useContext(ChatDisplayContext);
 
-    return (
-      <div className="chat__list__footer">
-        <button onClick={ () => changeDisplay(ChatType.FORM) }>
-          <CirclePlusIcon />  
-        </ button>
-      </div>
-    )
-  }
+  return (
+    <div className="chat__list__footer">
+      <button onClick={() => setDisplay(ChatType.FORM)}>
+        <CirclePlusIcon />
+      </button>
+    </div>
+  );
+}
 
-  export default MessageList;
+export default MessageList;
