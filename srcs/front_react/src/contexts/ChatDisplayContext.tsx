17dee273b5a6aff7b5c4226c2088aa838205dbbb
Guillaume PETIT <gpetit@e1r11p7.clusters.42paris.fr>
2022-11-10 13:54:57 +0100

    [save] midi

--- srcs/front_react/src/contexts/ChatDisplayContext.tsx
+++ srcs/front_react/src/contexts/ChatDisplayContext.tsx
@@ -10,15 +10,17 @@ export enum ChatType {
 
 export interface ChatDisplayContextInterface {
   display: ChatType;
-  setDisplay: (display: ChatType) => void;
+  setDisplay: React.Dispatch<React.SetStateAction<ChatType>>;
   currentConv: string;
-  setCurrentConv: (conv: string) => void;
+  setCurrentConv: React.Dispatch<React.SetStateAction<string>>
   isChannel: boolean;
-  setIsChannel: (isChannel: boolean) => void;
+  setIsChannel: React.Dispatch<React.SetStateAction<boolean>>;
   newConv: IChannel | IDm;
-  setNewConv: (newConv: IChannel | IDm) => void;
-  setTargetRedirection: (target: string) => void;
-  setRedirection: (redirection: boolean) => void;
+  setNewConv: React.Dispatch<React.SetStateAction<IChannel | IDm>>;
+  isRedirection: boolean;
+  setRedirection: React.Dispatch<React.SetStateAction<boolean>>;
+  targetRedirection: string;
+  setTargetRedirection: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export const ChatDisplayContext = createContext<ChatDisplayContextInterface>(
@@ -29,13 +31,13 @@ interface ChatDisplayProviderProps {
   children: JSX.Element | JSX.Element[];
 }
 
-export const ChatStateProvider = ({ children }: ChatDisplayProviderProps) => {
+export const ChatDisplayProvider = ({ children }: ChatDisplayProviderProps) => {
   const [display, setDisplay] = useState<ChatType>(ChatType.EMPTY);
   const [currentConv, setCurrentConv] = useState<string>("");
   const [isChannel, setIsChannel] = useState<boolean>(false);
   const [newConv, setNewConv] = useState<IChannel | IDm>({} as IChannel | IDm);
   const [isRedirection, setRedirection] = useState<boolean>(false);
-  const [targetRedirection, setTargetRedirection] = useState<string>();
+  const [targetRedirection, setTargetRedirection] = useState<string>("");
 
   return (
     <ChatDisplayContext.Provider
@@ -48,8 +50,10 @@ export const ChatStateProvider = ({ children }: ChatDisplayProviderProps) => {
         setIsChannel,
         newConv,
         setNewConv,
-        setTargetRedirection,
+        isRedirection,
         setRedirection,
+        targetRedirection,
+        setTargetRedirection,
       }}
     >
       {children}
