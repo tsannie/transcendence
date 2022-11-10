d5234486555d96d6ff6af167ad2f906b04c9c17e
tsannie <tsannie@student.42.fr>
2022-11-10 18:47:28 +0100

    [fix] nothing

--- srcs/front_react/src/components/chat/JoinChannelForm.tsx
+++ srcs/front_react/src/components/chat/JoinChannelForm.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { AxiosResponseHeaders } from "axios";
+import React, { useEffect, useState } from "react";
+import { api } from "../../const/const";
 import ChannelTable from "./ChannelTable";
+import { IChannel } from "./types";
 
 function JoinChannelForm() {
+  const [channelDictionnary, setChannelDictionnary] = useState<IChannel[]>([]);
+
+  useEffect(() => {
+    api
+      .get("/channel/list")
+      .then((res: AxiosResponseHeaders) => {
+        setChannelDictionnary(res.data);
+      })
+      .catch(() => console.log("Axios Error"));
+  }, []);
+
   return (
     <div className="channel_table">
       <ChannelTable />
