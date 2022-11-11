0f20cb0547700ad94b8ae2f12e305b4c88d13450
Guillaume PETIT <gpetit@e1r8p6.clusters.42paris.fr>
2022-11-11 13:22:17 +0100

    [+] patched redundant DM creation. Added draft message display. still buggy

--- srcs/front_react/src/components/chat/Channel.tsx
+++ srcs/front_react/src/components/chat/Channel.tsx
@@ -1,74 +1,10 @@
 import "./chat.style.scss";
 import { MessageContext } from "../../contexts/MessageContext";
-import { ChatDisplayContext } from "../../contexts/ChatDisplayContext";
 import { Fragment, useContext, useEffect, useRef, useState } from "react";
-import { IChannel, IDm, IMessageReceived, IMessageSent } from "./types";
+import { IMessageReceived } from "./types";
 import { api } from "../../const/const";
 import { AuthContext, AuthContextType, User } from "../../contexts/AuthContext";
-import { ReactComponent as SendIcon } from "../../assets/img/icon/send.svg";
-import { toast } from "react-toastify";
-
-function MessageForm(props: any) {
-  const convId = props.convId;
-  const isChannel = props.isChannel;
-
-  const { socket } = useContext(MessageContext);
-  const { isRedirection, setRedirection, targetRedirection, setNewConv, setCurrentConv } = useContext(ChatDisplayContext);
-  const [input, setInput] = useState<string>("");
-
-  const actualize_input = (event: any) => {
-    setInput(event.target.value);
-  };
-
-  const createConv = async () => {
-    let createdId : string | null = null;
-
-    await api
-      .post("/dm/create", {targetId: targetRedirection.toString()})
-      .then((res) => {
-        setRedirection(false);
-        setNewConv(res.data);
-        setCurrentConv(res.data.id);
-        createdId = res.data.id;
-      })
-      .catch((err) => {
-        toast.error("HTTP error: " + err);
-      })
-      return createdId;
-  }
-
-  const sendMessage = async (event: any) => {
-    let inputConvId : string | null = convId;
-  
-    event.preventDefault();
-    if (input === "") return;
-    if (isRedirection && targetRedirection)
-      inputConvId = await createConv();
-    if (!inputConvId)
-      return ;
-
-    const data: IMessageSent = {
-      convId: inputConvId,
-      content: input,
-      isDm: !isChannel,
-    };
-    socket?.emit("message", data);
-    setInput("");
-  };
-
-  return (
-    <form onSubmit={sendMessage}>
-      <input
-        className="input__form"
-        type="text"
-        placeholder="add message..."
-        value={input}
-        onChange={actualize_input}
-      />
-      <SendIcon className="send__button" onClick={sendMessage} />
-    </form>
-  );
-}
+import SendMessageForm from "./SendMessageForm";
 
 function MessageList(props: any) {
   const user: User = props.user;
@@ -119,7 +55,6 @@ function MessageBody(props: {currentConvId: string, isChannel: boolean}) {
 
     if (!currentConvId)
       return ;
-
     if (isChannel) route = "/message/channel";
     else route = "/message/dm";
 
@@ -169,7 +104,7 @@ function MessageBody(props: {currentConvId: string, isChannel: boolean}) {
           <MessageList messages={messages} user={user} />
           <div ref={messagesEndRef} />
         </ul>
-        <MessageForm convId={currentConvId} isChannel={isChannel} />
+        <SendMessageForm convId={currentConvId} isChannel={isChannel} />
       </div>
     </Fragment>
   );
