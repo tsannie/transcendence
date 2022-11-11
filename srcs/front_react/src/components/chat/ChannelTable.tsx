59ce8f84ae7fab9da409154572d3483818ef3483
tsannie <tsannie@student.42.fr>
2022-11-11 16:19:20 +0100

    [fix] start fix css

--- srcs/front_react/src/components/chat/ChannelTable.tsx
+++ srcs/front_react/src/components/chat/ChannelTable.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import { AxiosResponse } from "axios";
+import React, { useEffect, useState } from "react";
+import { api } from "../../const/const";
 import "./chat.style.scss";
+import { IChannel } from "./types";
 
-export type Data = {
-  name: string;
-  owner: string;
-  status: string;
-};
+interface IProps {
+  data: IChannel[];
+}
 
-function ChannelTable() {
+function ChannelTable(props: IProps) {
+  let allFriends = props.data.map((channel, index) => {
+    return (
+      <div className="table__item" key={channel.id}>
+        <div className="table__item__name">
+          <span>{channel.name}</span>
+        </div>
+        <div className="table__item__owner">
+          <span>{channel.owner?.username}</span>
+        </div>
+        <div className="table__item__status">
+          <span>{channel.status}</span>
+        </div>
+      </div>
+    );
+  });
   return (
     <div className="table">
       <div className="table__item">
@@ -21,6 +37,7 @@ function ChannelTable() {
           <span>Status</span>
         </div>
       </div>
+      <div className="table__body">{allFriends}</div>
     </div>
   );
 }
