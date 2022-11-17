dd47024407f3dcf6e30ec1d568c39ef9e63e67d4
Guillaume PETIT <gpetit@e1r9p2.clusters.42paris.fr>
2022-11-17 13:55:24 +0100

    [save] midi

--- srcs/front_react/src/components/chat/OptionsChannel.tsx
+++ srcs/front_react/src/components/chat/OptionsChannel.tsx
@@ -1,74 +1,10 @@
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Fragment } from "react";
 import { User } from "../../contexts/AuthContext";
 import { IDatas } from "./Conversation";
+import UserOptions from "./OptionsChannelActions";
 import { IChannel } from "./types";
 
-function UserOptions(props: {type: string, isOwner: boolean, isAdmin: boolean, user: User}) {
-    const {type, isOwner, isAdmin, user} = props;
-    const [isOpen, setOpen] = useState<boolean>(false);
-    
-    const adminOptions = () => {
-        let adminOptionsJSX: JSX.Element[] = [];
 
-        if (isOwner){    
-            adminOptionsJSX.push(<button>Ban User</button>);
-            adminOptionsJSX.push(<button>Mute User</button>);
-            adminOptionsJSX.push(<button>Revoke Admin</button>);
-        }
-        return <Fragment>{adminOptionsJSX}</Fragment>
-    }
-
-    const memberOptions = () => {
-        let adminOptionsJSX: JSX.Element[] = [];
-
-        if (isAdmin){    
-            adminOptionsJSX.push(<button>Ban User</button>);
-            adminOptionsJSX.push(<button>Mute User</button>);
-        }
-        if (isOwner)
-            adminOptionsJSX.push(<button>Make Admin</button>)
-        return <Fragment>{adminOptionsJSX}</Fragment>
-    }
-
-    const mutedOptions = () => {
-        let adminOptionsJSX: JSX.Element[] = [];
-
-        if (isAdmin || isOwner){    
-            adminOptionsJSX.push(<button>Unmute User</button>);
-            adminOptionsJSX.push(<button>Ban User</button>);
-        }
-        return <Fragment>{adminOptionsJSX}</Fragment>
-    }
-
-    const bannedOptions = () => {
-        let adminOptionsJSX: JSX.Element[] = [];
-
-        if (isAdmin || isOwner){    
-            adminOptionsJSX.push(<button>UnBan User</button>);
-        }
-        return <Fragment>{adminOptionsJSX}</Fragment>
-    }
-
-    const displayOptions = () => {
-        return (
-        <div className="dropdown">
-            <div className="options">Options</div>
-            <button>Access Profile</button>
-            {type === "Admins" && adminOptions()}
-            {type === "Members" && memberOptions()}
-            {type === "Muted" && mutedOptions()}
-            {type === "Banned" && bannedOptions()}
-        </div>);
-    }
-
-    return (
-    <Fragment>
-        <button className="member" onClick={(e: any) => {console.log(e); setOpen(true);}}>
-            <img src={user.profile_picture} />
-        </button>
-        {isOpen ? displayOptions() : null}
-    </Fragment>);
-}
 
 function MemberCategory(props: {name: string, isOwner: boolean, isAdmin: boolean, users: User[] | null}) {
     const { users } = props;
