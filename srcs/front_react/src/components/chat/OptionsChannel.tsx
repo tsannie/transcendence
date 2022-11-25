c5f3ecb1ac2f2f5092c25fd9df55c7ba2aeacc26
Guillaume PETIT <gpetit@e1r10p2.clusters.42paris.fr>
2022-11-25 21:09:17 +0100

    [+] trying to overflow the members div with options

--- srcs/front_react/src/components/chat/OptionsChannel.tsx
+++ srcs/front_react/src/components/chat/OptionsChannel.tsx
@@ -156,7 +156,7 @@ function ChannelMembers(props: {receivedChannel: IDatas, currentConvId: string})
     return (
         <Fragment>
         { channel ?
-        <div className="conversation__options__members" key={props.currentConvId} >
+        <div className="conversation__options__members" key={props.currentConvId} onClick={ (e) =>{ console.log("bubble2 =", (e.target as HTMLElement).getBoundingClientRect()); console.log((e.target as HTMLElement).getBoundingClientRect().top, (e.target as HTMLElement).getBoundingClientRect().left)}}>
             <MemberCategory type={"Admins"} isOwner={status === "owner"} isAdmin={status === "admin"} channelId={channel.id} users={admins}/>
             <MemberCategory type={"Members"} isOwner={status === "owner"} isAdmin={status === "admin"} channelId={channel.id} users={users as User[] | null}/>
             <MemberCategory type={"Muted"} isOwner={status === "owner"} isAdmin={status === "admin"} channelId={channel.id} users={muted as User[] | null}/>
