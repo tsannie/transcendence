185a06da1eb4b55616927f8164db3104c2995295
tsannie <tsannie@student.42.fr>
2022-11-26 08:57:06 +0100

    [+] searching

--- srcs/front_react/src/components/game/Game.tsx
+++ srcs/front_react/src/components/game/Game.tsx
@@ -1,7 +1,6 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import GameMenu from "./menu/Menu";
 import { GameRender } from "./render/Render";
-import WaitingRoom from "./menu/WaitingRoom";
 import { GameMode, RoomStatus } from "./const/const";
 import { GameContext, GameContextType } from "../../contexts/GameContext";
 import "./game.style.scss";
@@ -12,29 +11,11 @@ import GameDuel from "./menu/GameDuel";
 import GameCurrent from "./menu/GameCurrent";
 import GameContentHeader from "./menu/GameContentHeader";
 import GameMatchmaking from "./menu/GameMatchmaking";
+import GameWaiting from "./menu/GameWaiting";
 
 export default function Game() {
   const { socket, room } = useContext(GameContext) as GameContextType;
   const { user } = useContext(AuthContext) as AuthContextType;
-
-  /*if (room) {
-    if (room.status === RoomStatus.WAITING) {
-      return <WaitingRoom />;
-    } else {
-      return <GameRender />;
-    }
-  } else {
-    return <GameMenu />;
-  }*/
-
-  /*<div className="GameMenu">
-  <h2> you are : {user?.username} </h2>
-  <br />
-  <button onClick={matchmakingClassic}>pong classic</button>
-  <br />
-  <br />
-  <button onClick={matchmakingTrans}>pong transcendence</button>
-</div>*/
 
   return (
     <div className="game">
@@ -44,7 +25,11 @@ export default function Game() {
       </div>
       <div className="game__content">
         <GameContentHeader />
-        <GameMatchmaking />
+        {room?.status !== RoomStatus.WAITING ? (
+          <GameMatchmaking />
+        ) : (
+          <GameWaiting />
+        )}
       </div>
     </div>
   );
