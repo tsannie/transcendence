48a1fe222eda61bdffece1773addd9dfdb85c123
tsannie <tsannie@student.42.fr>
2022-11-26 20:15:30 +0100

    [fix] style history

--- srcs/front_react/src/components/profile/ProfileHistory.tsx
+++ srcs/front_react/src/components/profile/ProfileHistory.tsx
@@ -33,57 +33,53 @@ function ProfileHistory(props: IProps) {
       // console.log("game", game);
       return (
         <Fragment key={index}>
-          <li className="profile__body__history__list">
-            <div className="profile__body__history__item">
-              <div className="trophy-indicator">
+          <div className="profile__body__history__item">
+            <div className="trophy-indicator">
+              {(game.winner === Winner.P1 && game.p1.id === props.player?.id) ||
+              (game.winner === Winner.P2 && game.p2.id === props.player?.id) ? (
+                <TrophyIcon />
+              ) : null}
+            </div>
+            <div className="info">
+              <span>
                 {(game.winner === Winner.P1 &&
                   game.p1.id === props.player?.id) ||
-                (game.winner === Winner.P2 &&
-                  game.p2.id === props.player?.id) ? (
-                  <TrophyIcon />
-                ) : null}
-              </div>
-              <div className="info">
-                <span>
-                  {(game.winner === Winner.P1 &&
-                    game.p1.id === props.player?.id) ||
-                  (game.winner === Winner.P2 && game.p2.id === props.player?.id)
-                    ? "victory"
-                    : "defeat"}
-                </span>
-                <div className="info__elo">
-                  {(game.winner === Winner.P1 &&
-                    game.p1.id === props.player?.id) ||
-                  (game.winner === Winner.P2 && game.p2.id === props.player?.id)
-                    ? game.eloDiff
-                    : -game.eloDiff}
-                </div>
-              </div>
-              <Link
-                to={
-                  "/profile/" +
-                  (game.p1.id === props.player?.id
-                    ? game.p2.username
-                    : game.p1.username)
-                }
-              >
-                <img
-                  alt="avatar"
-                  src={
-                    props.player?.id === game.p1.id
-                      ? game.p2.profile_picture
-                      : game.p1.profile_picture
-                  }
-                />
-              </Link>
-
-              <span>
-                {props.player?.id === game.p1.id
-                  ? game.p1_score + " - " + game.p2_score
-                  : game.p2_score + " - " + game.p1_score}
+                (game.winner === Winner.P2 && game.p2.id === props.player?.id)
+                  ? "victory"
+                  : "defeat"}
               </span>
+              <div className="info__elo">
+                {(game.winner === Winner.P1 &&
+                  game.p1.id === props.player?.id) ||
+                (game.winner === Winner.P2 && game.p2.id === props.player?.id)
+                  ? game.eloDiff
+                  : -game.eloDiff}
+              </div>
             </div>
-          </li>
+            <Link
+              to={
+                "/profile/" +
+                (game.p1.id === props.player?.id
+                  ? game.p2.username
+                  : game.p1.username)
+              }
+            >
+              <img
+                alt="avatar"
+                src={
+                  props.player?.id === game.p1.id
+                    ? game.p2.profile_picture
+                    : game.p1.profile_picture
+                }
+              />
+            </Link>
+
+            <span>
+              {props.player?.id === game.p1.id
+                ? game.p1_score + " - " + game.p2_score
+                : game.p2_score + " - " + game.p1_score}
+            </span>
+          </div>
           <hr />
         </Fragment>
       );
@@ -95,7 +91,9 @@ function ProfileHistory(props: IProps) {
         <h3>recent games</h3>
       </div>
       <hr id="full" />
-      <div>{allHistory?.reverse()}</div>
+      <div className="profile__body__history__list">
+        {allHistory?.reverse()}
+      </div>
     </div>
   );
 }
