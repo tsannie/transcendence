c883fded227c7b9cc39433ca083fe8a31c55f904
tsannie <tsannie@student.42.fr>
2022-11-26 10:46:48 +0100

    [+] start current games

--- srcs/back_nestjs/src/game/controller/game.controller.ts
+++ srcs/back_nestjs/src/game/controller/game.controller.ts
@@ -1,17 +1,29 @@
 import {
+  ClassSerializerInterceptor,
   Controller,
   Get,
   Param,
   Query,
   Request,
+  SerializeOptions,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import JwtTwoFactorGuard from 'src/auth/guard/jwtTwoFactor.guard';
+import { IInfoRoom } from '../class/room.class';
 import { GameService } from '../service/game.service';
 
 @Controller('game')
+@UseInterceptors(ClassSerializerInterceptor)
 export class GameController {
   constructor(private gameService: GameService) {}
+
+  @Get('rooms')
+  @SerializeOptions({ groups: ['user'] })
+  @UseGuards(JwtTwoFactorGuard)
+  async getRoom(@Request() req: Request): Promise<IInfoRoom[]> {
+    return this.gameService.getCurrentRooms();
+  }
 
   /*@Get()
   async index(): Promise<RoomEntity[]> {
