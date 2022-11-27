c0c55577a0a715cc70d330d1ba6d05caeeff89b7
Guillaume Petit <gpetit@student.42.fr>
2022-11-27 18:42:04 +0100

    [+] BanMute working backside, need to actualize status through websocket

--- srcs/front_react/src/components/chat/OptionsBanMute.tsx
+++ srcs/front_react/src/components/chat/OptionsBanMute.tsx
@@ -1,8 +1,46 @@
 import { Fragment, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../../const/const";
+import { User } from "../../contexts/AuthContext";
 
-function BanMuteButton(props: {key: number, type : string, onClickEvent: () => Promise<void>}) {
+export interface IMuteBan {
+    id: string,
+    targetId: string,
+    duration: number,
+}
+
+function BanMuteButton(props: {key: number, type : string, channelId: string, user : User}) {
+    const { key, type, channelId, user } = props;
     const [ timer, setTimer ] = useState<number>(0);
-    const [timerOption, setTimerOption ] = useState<boolean>(false);
+    const [ timerOption, setTimerOption ] = useState<boolean>(false);
+
+    const muteUser = async () => {
+        let data : IMuteBan | Partial<IMuteBan>;
+
+        if (timer === 0)
+            data = {id: channelId, targetId: user.id};
+        else
+            data = {id: channelId, targetId: user.id, duration: timer};
+
+        await api
+        .post("/channel/mute", data)
+        .then(() => toast.info(`${user.username} has been muted`))
+        .catch((error: any) => toast.error("HTTP error:" + error));
+    }
+
+    const banUser = async () => {
+        let data : IMuteBan | Partial<IMuteBan>;
+
+        if (timer === 0)
+            data = {id: channelId, targetId: user.id};
+        else
+            data = {id: channelId, targetId: user.id, duration: timer};
+
+        await api
+        .post("/channel/ban", data)
+        .then(() => toast.info(`${user.username} has been banned`))
+        .catch((error: any) => toast.error("HTTP error:" + error));
+    }
 
     useEffect( () => {
         setTimerOption(false);
@@ -13,18 +51,30 @@ function BanMuteButton(props: {key: number, type : string, onClickEvent: () => P
         setTimerOption(true);
     }
 
+    const handleAction = (event: any) => {
+        event.preventDefault();
+        if (type === "Mute")
+            muteUser();
+        else
+            banUser();
+        setTimer(0);
+        setTimerOption(false);
+    }
+
     return (
         <Fragment>
             {
                 !timerOption ? 
-                <button key={props.key} onClick={createTimer}>Mute</button>
+                <button key={key} onClick={createTimer}>{type}</button>
                 : 
                 <Fragment>
-                    <button key={props.key}>Mute</button>
-                    <form onSubmit={props.onClickEvent}>
+                    <button key={key}>{type}</button>
+                    <form onSubmit={handleAction}>
                         <input
                             type="number"
+                            min="0"
                             placeholder="minutes"
+                            value={timer}
                             onChange={(e: any) =>
                                 setTimer(e.target.value)
                             }
