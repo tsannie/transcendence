2b7cd160ef5bc3ac009ca7927878e8be23f52c2b
tsannie <tsannie@student.42.fr>
2022-11-27 12:03:35 +0100

    [fix] front

--- srcs/front_react/src/components/menu/PageNotFound.tsx
+++ srcs/front_react/src/components/menu/PageNotFound.tsx
@@ -1,13 +1,14 @@
 import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { AuthContext, AuthContextType } from "../../contexts/AuthContext";
 
 function PageNotFound() {
   const nav = useNavigate();
 
   useEffect(() => {
     toast.warning("page not found !");
-    nav("/");
+    nav("/game");
   }, []);
   return <>redirection</>;
 }
