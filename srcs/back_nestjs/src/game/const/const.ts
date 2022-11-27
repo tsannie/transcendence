562c3a390bb38b4732997c903c6c38435da4eb33
tsannie <tsannie@student.42.fr>
2022-11-27 12:31:02 +0100

    [fix] pseudo to long

--- srcs/back_nestjs/src/game/const/const.ts
+++ srcs/back_nestjs/src/game/const/const.ts
@@ -2,7 +2,7 @@ export const canvas_back_width: number = 1920;
 export const screen_ratio: number = 9 / 16;
 export const canvas_back_height: number = canvas_back_width * screen_ratio;
 
-export const victory_score: number = 2;
+export const victory_score: number = 200;
 
 // BALL DATA
 export const rad: number = 20;
