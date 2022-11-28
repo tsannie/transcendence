40c000eba8b3c87eb433110a011e11ec851848c3
tsannie <tsannie@student.42.fr>
2022-11-28 16:23:36 +0100

    [+] invitations

--- srcs/front_react/src/components/game/menu/GameAmical.tsx
+++ srcs/front_react/src/components/game/menu/GameAmical.tsx
@@ -2,8 +2,7 @@ import { AxiosError, AxiosResponse } from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { api } from "../../../const/const";
 import { User } from "../../../contexts/AuthContext";
-import { ReactComponent as BallIcon } from "../../../assets/img/icon/full_ball.svg";
-import { ReactComponent as BallTransIcon } from "../../../assets/img/icon/ball-reverse.svg";
+import { ReactComponent as BallIcon } from "../../../assets/img/icon/ball-reverse.svg";
 import { GameContext, GameContextType } from "../../../contexts/GameContext";
 import { Link } from "react-router-dom";
 import { GameMode } from "../const/const";
@@ -15,7 +14,8 @@ function GameAmical() {
     GameContext
   ) as GameContextType;
 
-  let allFriends: JSX.Element[];
+  let allFriends: JSX.Element[] = [];
+  let allInvitations: JSX.Element[] = [];
 
   const handleInvite = (friend_id: string, mode: GameMode) => {
     const data: ICreateRoom = {
@@ -39,7 +39,56 @@ function GameAmical() {
   };
 
   if (friendsLog.length) {
+    // sort friends by username
+
+    allInvitations = inviteReceived.map((invite) => {
+      const friend = friendsLog.find((f) => f.id === invite.user_id);
+      if (friend) {
+        return (
+          <div className="duel__list__item" key={friend.id}>
+            <div className="duel__list__item__info">
+              <Link to={`/profile/${friend.username}`}>
+                <button>
+                  <img src={friend.profile_picture} alt="avatar" />
+                </button>
+              </Link>
+              <span title={friend.username.length > 10 ? friend.username : ""}>
+                {friend.username.length > 10
+                  ? friend.username.slice(0, 7) + "..."
+                  : friend.username}
+              </span>
+            </div>
+            <div className="duel__list__item__action">
+              <button
+                id={invite.mode === GameMode.PONG_CLASSIC ? "classic" : "trans"}
+                title="Accept"
+                //onClick={() => handleAccept(friend.id)}
+              >
+                <BallIcon />
+              </button>
+              <button
+                id={invite.mode === GameMode.PONG_CLASSIC ? "classic" : "trans"}
+                title="Decline"
+                //onClick={() => handleDecline(friend.id)}
+              >
+                <BallIcon />
+              </button>
+            </div>
+          </div>
+        );
+      } else {
+        return <></>;
+      }
+    });
+
+    friendsLog.sort((a, b) => {
+      if (a.username < b.username) return -1;
+      if (a.username > b.username) return 1;
+      return 0;
+    });
     allFriends = friendsLog.map((friend: User) => {
+      // check if user is already in the list of invitations
+      if (inviteReceived.some((inv) => inv.user_id === friend.id)) return <></>;
       return (
         <div className="duel__list__item" key={friend.id}>
           <div className="duel__list__item__info">
@@ -67,7 +116,7 @@ function GameAmical() {
               title="Invite in trans mode"
               onClick={() => handleInvite(friend.id, GameMode.PONG_TRANS)}
             >
-              <BallTransIcon />
+              <BallIcon />
             </button>
           </div>
         </div>
@@ -86,7 +135,10 @@ function GameAmical() {
       <div className="game__menu__item__header">
         <h2>amical</h2>
       </div>
-      <div className="duel__list">{allFriends}</div>
+      <div className="duel__list">
+        {allInvitations}
+        {allFriends}
+      </div>
     </div>
   );
 }
