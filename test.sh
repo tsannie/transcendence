595ab4f710921257a48cf45d87c3a7b32c461155
tsannie <tsannie@student.42.fr>
2022-12-02 19:18:18 +0100

    [+] project

--- test.sh
+++
@@ -1,5 +0,0 @@
-curl -F grant_type=client_credentials \
--F client_id=16312ad4625ac63660e140ef9cafdc36ed82512a2310f60e2cbeceaa906bf655 \
--F client_secret=80a45ff10c0a8fe3f758bc3e7ecc9a04866e21ce5be8751cc1a84677d0ab8b7d \
--F code=3f8780c7d064aca06e240c14d873c8bd6458a7fd521b0e580182c8902feb0000 \
--X POST https://api.intra.42.fr/oauth/token
