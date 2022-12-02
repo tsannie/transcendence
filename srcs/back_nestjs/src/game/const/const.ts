19d60f25ead32ba825845c7a3082354e62710d5d
tsannie <tsannie@student.42.fr>
2022-12-02 12:10:34 +0100

    [fix] const

--- srcs/back_nestjs/src/game/const/const.ts
+++ srcs/back_nestjs/src/game/const/const.ts
@@ -2,14 +2,14 @@ export const canvas_back_width: number = 1920;
 export const canvas_back_height: number = (canvas_back_width * 9) / 16;
 export const frame_ms = 1000 / 60; // 60 fps
 
-export const victory_score: number = 200;
+export const victory_score: number = 10;
 
 // BALL DATA
 export const rad: number = 20;
-export const gravity: number = 5;
+export const gravity: number = 15;
 export const speed_spawn: number = 10;
 export const speed: number = 25;
-export const speed_smasher: number = 35;
+export const speed_smasher: number = 40;
 
 // PADDLE DATA
 export const paddle_margin: number = 100;
