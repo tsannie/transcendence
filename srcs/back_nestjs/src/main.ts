098dd21fccdcf35f30009e967205f0a4775490bb
tsannie <tsannie@student.42.fr>
2022-12-09 16:44:33 +0100

    [+] nest /api

--- srcs/back_nestjs/src/main.ts
+++ srcs/back_nestjs/src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
+  app.setGlobalPrefix('api');
 
   app.use((req, res, next) => {
     next();
